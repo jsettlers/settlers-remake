8ac11126f728075933befa72ea671f577832a5a8
Michael Zangl <michael>
2011-10-20 19:39:50 +0000

    Added minimap

--- src/jsettlers/common/landscape/ELandscapeType.java
+++ src/jsettlers/common/landscape/ELandscapeType.java
@@ -1,28 +1,41 @@
 package jsettlers.common.landscape;
 
+import java.awt.Color;
+
 public enum ELandscapeType {
-	GRASS(0),
-	DRY_GRASS(1),
-	DESERT(3),
-	EARTH(2),
-	MOUNTAIN(21),
-	SNOW(24),
-	WATER(10),
-	SAND(3),
-	FLATTENED(35),
-	RIVER1(10),
-	RIVER2(10),
-	RIVER3(10),
-	RIVER4(10), MOUNTAINBORDER(21);
+	GRASS(0, new Color(0x3f9b0b)),
+	DRY_GRASS(1, new Color(0x5cac2d)),
+	DESERT(3, new Color(0xccad60)),
+	EARTH(2, new Color(0xa2653e)),
+	MOUNTAIN(21, new Color(0x59656d)),
+	SNOW(24, new Color(0xd7fffe)),
+	WATER(10, new Color(0x0e87cc)),
+	SAND(3, new Color(0xccad60)),
+	FLATTENED(35, new Color(0xcfaf7b)),
+	RIVER1(10, new Color(0x0e87cc)),
+	RIVER2(10, new Color(0x0e87cc)),
+	RIVER3(10, new Color(0x0e87cc)),
+	RIVER4(10, new Color(0x0e87cc)), 
+	MOUNTAINBORDER(21, new Color(0x59656d));
+	
+	private final int image;
+	private final Color color;
 
-	private int image;
-
-	ELandscapeType(int image) {
+	ELandscapeType(int image, Color color) {
 		this.image = image;
+		this.color = color;
 	}
 	
 	public int getImageNumber() {
 	    return image;
+    }
+
+	/**
+	 * Gets the base color of the landscape
+	 * @return The color the landscape has.
+	 */
+	public Color getColor() {
+	    return color;
     }
 
 }
