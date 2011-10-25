79ee0c3e386b3ddef0bb10f8c84ee8d865110839
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-10-25 18:23:02 +0000

    - still buggy management - FIXED bug with DijkstraAlgorithm that lead to found positions that couldn't be reached - fixed some bugs in Management

--- src/jsettlers/common/Color.java
+++ src/jsettlers/common/Color.java
@@ -2,10 +2,13 @@ package jsettlers.common;
 
 public class Color extends go.graphics.Color {
 
+	public static final Color BLUE = new Color(0, 0, 1, 1);
+	public static final Color RED = new Color(1, 0, 0, 1);
+
 	public Color(float red, float green, float blue, float alpha) {
-	    super(red, green, blue, alpha);
-    }
-	
+		super(red, green, blue, alpha);
+	}
+
 	public Color(int i) {
 		super(i);
 	}
