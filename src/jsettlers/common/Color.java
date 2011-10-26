b16a7187b4aea81ca412f0137bbdc639fc231535
Michael Zangl <michael>
2011-10-26 09:35:02 +0000

    Updated image loading to dynamically load images as needed

--- src/jsettlers/common/Color.java
+++ src/jsettlers/common/Color.java
@@ -1,6 +1,12 @@
 package jsettlers.common;
 
 public class Color extends go.graphics.Color {
+	public static final Color BLACK = new Color(0, 0, 0, 1);
+	public static final Color WHITE = new Color(1, 1, 1, 1);
+	public static final Color TRANSPARENT = new Color(1, 1, 1, 0);
+	public static final Color RED = new Color(1, 0, 0, 1);
+	public static final Color BLUE = new Color(0, 0, 1, 1);
+	public static final Color GREEN = new Color(0, 1, 0, 1);
 
 	public static final Color BLUE = new Color(0, 0, 1, 1);
 	public static final Color RED = new Color(1, 0, 0, 1);
