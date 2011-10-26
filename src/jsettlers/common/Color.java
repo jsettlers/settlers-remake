78e66398ff7cdbace3d9f99751e3339da05f1f61
Michael Zangl <michael>
2011-10-26 10:19:22 +0000

    Minor debug output cleanup, made random map generation more robust.

--- src/jsettlers/common/Color.java
+++ src/jsettlers/common/Color.java
@@ -8,9 +8,6 @@ public class Color extends go.graphics.Color {
 	public static final Color BLUE = new Color(0, 0, 1, 1);
 	public static final Color GREEN = new Color(0, 1, 0, 1);
 
-	public static final Color BLUE = new Color(0, 0, 1, 1);
-	public static final Color RED = new Color(1, 0, 0, 1);
-
 	public Color(float red, float green, float blue, float alpha) {
 		super(red, green, blue, alpha);
 	}
