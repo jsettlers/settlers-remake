fcc8c9386730aeedf401f9d1c69854eb2a96ea4e
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-10-26 17:46:10 +0000

    - made JSettlersActivity keep the screen on - moved displayed area to correct position

--- src/go/graphics/android/GOSurfaceView.java
+++ src/go/graphics/android/GOSurfaceView.java
@@ -11,64 +11,66 @@ import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
 
 public class GOSurfaceView extends GLSurfaceView implements RedrawListener {
-	
+
 	private final Area area;
 
 	public GOSurfaceView(Context context, Area area) {
-	    super(context);
+		super(context);
 		this.area = area;
 
-        setRenderer(new Renderer());
-        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+		setRenderer(new Renderer());
+		// setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 
-        area.addRedrawListener(this);
-    }
-    @Override 
-    public boolean onTouchEvent(MotionEvent e) {
-//        float x = e.getX();
-//        float y = e.getY();
-
-        return true;
-    }
-    
-	private class Renderer implements GLSurfaceView.Renderer {
-		private AndroidContext context;
-		
-		@Override
-        public void onDrawFrame(GL10 gl) {
-	        generateContext(gl);
-	        gl.glClearColor(1, 1, 0, 1);
-	        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	        
-	        area.drawArea(context);
-        }
-
-		private void generateContext(GL10 gl) {
-	        if (context == null) {
-	        	context = new AndroidContext(gl);
-	        }
-        }
-
-		@Override
-        public void onSurfaceChanged(GL10 gl, int width, int height) {
-	        generateContext(gl);
-	        area.setWidth(width);
-	        area.setHeight(height);
-	        gl.glViewport(width/2, height/2, width, height);
-	        System.out.println("Drawing started");
-        }
-
-		@Override
-        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-	        generateContext(gl);
-	        System.out.println("Drawing created");
-        }
-		
+		area.addRedrawListener(this);
 	}
 
 	@Override
-    public void requestRedraw() {
-	    requestRender();
-    }
+	public boolean onTouchEvent(MotionEvent e) {
+		// float x = e.getX();
+		// float y = e.getY();
+
+		return true;
+	}
+
+	private class Renderer implements GLSurfaceView.Renderer {
+		private AndroidContext context;
+
+		@Override
+		public void onDrawFrame(GL10 gl) {
+			generateContext(gl);
+			gl.glClearColor(1, 1, 0, 1);
+			gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+			area.drawArea(context);
+		}
+
+		private void generateContext(GL10 gl) {
+			if (context == null) {
+				context = new AndroidContext(gl);
+			}
+		}
+
+		@Override
+		public void onSurfaceChanged(GL10 gl, int width, int height) {
+			generateContext(gl);
+			area.setWidth(width);
+			area.setHeight(height);
+			gl.glViewport(0, 0, width, height);
+			gl.glTranslatef(-width / 2, -height / 2, 0);
+			System.out.println("Drawing started");
+		}
+
+		@Override
+		public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+			generateContext(gl);
+			System.out.println("Drawing created");
+		}
+
+	}
+
+	@Override
+	public void requestRedraw() {
+		requestRender();
+	}
 
 }
