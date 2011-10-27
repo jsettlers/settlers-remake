8d5a0069fa5a53d1123f3f6026884a77008bd94c
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-10-27 19:39:32 +0000

    - uncommented loading of previous user actions

--- src/jsettlers/main/swing/JSettlersApp.java
+++ src/jsettlers/main/swing/JSettlersApp.java
@@ -3,25 +3,22 @@ package jsettlers.main.swing;
 import go.graphics.swing.AreaContainer;
 
 import java.awt.Dimension;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import javax.swing.JFrame;
 
 import jsettlers.common.resources.ResourceManager;
 import jsettlers.graphics.JOGLPanel;
 import jsettlers.graphics.JoglLibraryPathInitializer;
-import synchronic.timer.NetworkTimer;
 
 public class JSettlersApp {
 	static { // sets the native library path for the system dependent jogl libs
 		JoglLibraryPathInitializer.initLibraryPath();
 	}
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+	public static void main(String[] args) {
 		ResourceManager.setProvider(new ResourceProvider());
 		new Thread(new SettlersGame()).start();
-		NetworkTimer.loadLogging("logs/2011_10_25-20_35_36.log");
+		// NetworkTimer.loadLogging("logs/2011_10_25-20_35_36.log");
 		// NetworkTimer.activateLogging("logs");
 	}
 
