d1267c162ef6e27f87821bc70c796ea83bd2e19c
Michael Zangl <michael>
2011-10-27 19:38:19 +0000

    Updated android loader, added some debug output

--- jsettlers.logic.map/jsettlers/logic/map/random/RandomMapFile.java
+++ jsettlers.logic.map/jsettlers/logic/map/random/RandomMapFile.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
