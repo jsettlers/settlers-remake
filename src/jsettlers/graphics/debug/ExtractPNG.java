76d9b715baa4bbfea6fae16128f0ee015df8c1ce
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-10-31 23:41:55 +0000

    - removed empty println

--- src/jsettlers/graphics/debug/ExtractPNG.java
+++ src/jsettlers/graphics/debug/ExtractPNG.java
@@ -1,12 +1,14 @@
 package jsettlers.graphics.debug;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
  * This file extracts PNGs out of a file.
+ * 
  * @author michael
- *
+ * 
  */
 public class ExtractPNG {
 
@@ -14,8 +16,7 @@ public class ExtractPNG {
 
 	private static byte[] START = { (byte) 0x89, 0x50, 0x4e, 0x47 };
 
-	private static byte[] END =
-	        { 0x45, 0x4e, 0x44, (byte) 0xae, 0x42, 0x60, (byte) 0x82 };
+	private static byte[] END = { 0x45, 0x4e, 0x44, (byte) 0xae, 0x42, 0x60, (byte) 0x82 };
 
 	/**
 	 * @param args
@@ -40,26 +41,24 @@ public class ExtractPNG {
 			nextImageFound = start >= 0 && end >= 7;
 
 			if (nextImageFound) {
-				System.out.println("Start: " + start + ", end: " + end
-				        + ", name: " + NAME + "-" + imageNumber + ".png");
+				System.out.println("Start: " + start + ", end: " + end + ", name: " + NAME + "-" + imageNumber + ".png");
 
-				
-//				File out =
-//				        new File("/media/2F35-CAAF/" + NAME + "-" + imageNumber
-//				                + ".png, data:");
+				// File out =
+				// new File("/media/2F35-CAAF/" + NAME + "-" + imageNumber
+				// + ".png, data:");
 				byte[] outData = new byte[end - start];
 
-				hexdumpLine(file, start-12, 12);
-				
+				hexdumpLine(file, start - 12, 12);
+
 				FileInputStream in = new FileInputStream(file);
 				in.skip(start);
 				in.read(outData);
 				in.close();
 
-//				
-//				FileOutputStream outstream = new FileOutputStream(out);
-//				outstream.write(outData);
-//				outstream.close();
+				//
+				// FileOutputStream outstream = new FileOutputStream(out);
+				// outstream.write(outData);
+				// outstream.close();
 
 				imageNumber++;
 			}
@@ -77,8 +76,7 @@ public class ExtractPNG {
 		return -1;
 	}
 
-	private static boolean compareBytes(byte[] toSearch, byte[] original,
-	        int originalOffset) {
+	private static boolean compareBytes(byte[] toSearch, byte[] original, int originalOffset) {
 		for (int i = 0; i < toSearch.length; i++) {
 			if (toSearch[i] != original[i + originalOffset]) {
 				return false;
@@ -86,20 +84,19 @@ public class ExtractPNG {
 		}
 		return true;
 	}
-	
+
 	private static void hexdumpLine(File file, int offset, int len) throws IOException {
 		byte[] bytes = new byte[len];
-		
+
 		FileInputStream stream = new FileInputStream(file);
 		stream.skip(offset);
 		stream.read(bytes);
 		stream.close();
-		
+
 		for (int i = 0; i < len; i++) {
 			String asHex = Integer.toHexString(bytes[i]);
 			System.out.print(asHex + " ");
 		}
-		System.out.println("");
 	}
 
 }
