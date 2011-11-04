8b269228b1edb69e5b7659dc38e88d2d7914621b
Michael Zangl <michael>
2011-11-04 15:26:16 +0000

    Added Pigfarm Converted building job parser to Sax.

--- src/jsettlers/graphics/debug/DatFileTester.java
+++ src/jsettlers/graphics/debug/DatFileTester.java
@@ -46,7 +46,7 @@ public class DatFileTester {
 	// private static final String FILE =
 	// "D:/Games/Siedler3/GFX/siedler3_%.7c003e01f.dat";
 
-	private static int datFileIndex = 11;
+	private static int datFileIndex = 6;
 
 	private AdvancedDatFileReader reader;
 
