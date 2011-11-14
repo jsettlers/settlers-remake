56c1fa2d2e6c8cf3eb61caf25ff3dfe5962930c6
Michael Zangl <michael>
2011-11-14 20:13:59 +0000

    Made resource provider provide an output stream.

--- src/jsettlers/main/swing/ResourceProvider.java
+++ src/jsettlers/main/swing/ResourceProvider.java
@@ -2,8 +2,10 @@ package jsettlers.main.swing;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import jsettlers.common.resources.IResourceProvider;
 
@@ -21,6 +23,12 @@ public class ResourceProvider implements IResourceProvider {
     public InputStream getFile(String name) throws IOException {
 		File file = new File(path + name);
 	    return new FileInputStream(file);
+    }
+
+	@Override
+    public OutputStream writeFile(String name) throws IOException {
+		File file = new File(path + name);
+	    return new FileOutputStream(file);
     }
 	
 }
