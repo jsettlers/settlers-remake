562c0e1dcce6a8c32e144de69a78167cdaf8f2b7
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-12-13 14:58:15 +0000

    - added method to IGraphicsGrid to be able to set a listener for background changes

--- src/jsettlers/mapcreator/mapview/MapGraphics.java
+++ src/jsettlers/mapcreator/mapview/MapGraphics.java
@@ -2,82 +2,88 @@ package jsettlers.mapcreator.mapview;
 
 import jsettlers.common.Color;
 import jsettlers.common.landscape.ELandscapeType;
+import jsettlers.common.map.IGraphicsBackgroundListener;
 import jsettlers.common.map.IGraphicsGrid;
 import jsettlers.common.mapobject.IMapObject;
 import jsettlers.common.movable.IMovable;
 import jsettlers.mapcreator.data.MapData;
 
 public class MapGraphics implements IGraphicsGrid {
-	
 
 	private final MapData data;
 
 	public MapGraphics(MapData data) {
 		this.data = data;
 	}
-	
-	@Override
-    public short getHeight() {
-	    return (short) data.getWidth();
-    }
 
 	@Override
-    public short getWidth() {
+	public short getHeight() {
+		return (short) data.getWidth();
+	}
+
+	@Override
+	public short getWidth() {
 		return (short) data.getHeight();
-    }
+	}
 
 	@Override
-    public IMovable getMovableAt(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public IMovable getMovableAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public IMapObject getMapObjectsAt(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public IMapObject getMapObjectsAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public byte getHeightAt(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return 0;
-    }
+	public byte getHeightAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
-    public ELandscapeType getLandscapeTypeAt(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public ELandscapeType getLandscapeTypeAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public Color getDebugColorAt(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public Color getDebugColorAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public boolean isBorder(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return false;
-    }
+	public boolean isBorder(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	@Override
-    public byte getPlayerAt(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return 0;
-    }
+	public byte getPlayerAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
-    public byte getVisibleStatus(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return 0;
-    }
+	public byte getVisibleStatus(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
-    public boolean isFogOfWarVisible(int x, int y) {
-	    // TODO Auto-generated method stub
-	    return false;
-    }
-	
+	public boolean isFogOfWarVisible(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setBackgroundListener(IGraphicsBackgroundListener backgroundListener) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
