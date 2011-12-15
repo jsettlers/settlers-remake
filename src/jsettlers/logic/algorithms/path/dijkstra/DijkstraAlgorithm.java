5c9829dc3ac6f1f469482bcc37df0628a65d19a0
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-12-15 17:04:04 +0000

    - towers can now request movables - dijkstra can be executed partly - for testing every tower requests two bowmans - every tower initially placed on the map will be occupied by one swordsman

--- src/jsettlers/logic/algorithms/path/dijkstra/DijkstraAlgorithm.java
+++ src/jsettlers/logic/algorithms/path/dijkstra/DijkstraAlgorithm.java
@@ -15,19 +15,20 @@ import jsettlers.logic.algorithms.path.astar.HexAStar;
  * 
  */
 public final class DijkstraAlgorithm {
+	private static final byte[] directionIncreaseX = { -1, 0, 1, 1, 0, -1 };
+	private static final byte[] directionIncreaseY = { 0, 1, 1, 0, -1, -1 };
+	private static final float MAX_RADIUS_MULTIPLIER = 1f / MapCircle.Y_SCALE;
 
 	private final IDijkstraPathMap map;
 	private final short height, width;
 	private final HexAStar aStar;
 
-	private static final byte[] directionIncreaseX = { -1, 0, 1, 1, 0, -1 };
-	private static final byte[] directionIncreaseY = { 0, 1, 1, 0, -1, -1 };
-
 	public DijkstraAlgorithm(IDijkstraPathMap map, HexAStar aStar) {
 		this.map = map;
 		this.aStar = aStar;
-		height = map.getHeight();
-		width = map.getWidth();
+		this.height = map.getHeight();
+		this.width = map.getWidth();
+
 	}
 
 	public final Path find(final IPathCalculateable requester, final short cX, final short cY, final short minRadius, final short maxRadius,
@@ -36,8 +37,9 @@ public final class DijkstraAlgorithm {
 			throw new InvalidStartPositionException("dijkstra center position is not in bounds!", cX, cY);
 		}
 
-		MapCircle circle = new MapCircle(cX, cY, maxRadius / MapCircle.Y_SCALE);
 		// TODO @Michael find out how much the radius must be bigger to be larger than the circle
+		MapCircle circle = new MapCircle(cX, cY, maxRadius * MAX_RADIUS_MULTIPLIER);
+
 		for (short radius = minRadius; radius < maxRadius; radius++) {
 			short x = cX, y = (short) (cY - radius);
 			for (byte direction = 0; direction < 6; direction++) {
@@ -49,7 +51,7 @@ public final class DijkstraAlgorithm {
 					if (circle.contains(x, y) && isInBounds(x, y)) {
 						map.setDijkstraSearched(x, y);
 						if (map.fitsSearchType(x, y, type, requester)) {
-							Path path = findPath(requester, x, y);
+							Path path = findPathTo(requester, x, y);
 							if (path != null)
 								return path;
 						}
@@ -61,12 +63,86 @@ public final class DijkstraAlgorithm {
 		return null;
 	}
 
-	private final Path findPath(IPathCalculateable requester, short tx, short ty) {
+	private final Path findPathTo(IPathCalculateable requester, short tx, short ty) {
 		ISPosition2D pos = requester.getPos();
 		return aStar.findPath(requester, pos.getX(), pos.getY(), tx, ty);
 	}
 
 	private final boolean isInBounds(short x, short y) {
 		return 0 <= x && x < width && 0 <= y && y < height;
+	}
+
+	public static class DijkstraContinuableRequest {
+		final short minRadius;
+		final short maxRadius;
+		final IPathCalculateable requester;
+		final short cX;
+		final short cY;
+		final ESearchType searchType;
+
+		short radius;
+
+		public DijkstraContinuableRequest(final IPathCalculateable requester, short cX, short cY, short minRadius, short maxRadius,
+				ESearchType searchType) {
+			this.requester = requester;
+			this.cX = cX;
+			this.cY = cY;
+			this.minRadius = minRadius;
+			this.maxRadius = maxRadius;
+			this.searchType = searchType;
+
+			this.radius = 0;
+		}
+
+		final short getRadiusSteps() {
+			return 3;
+		}
+
+		public boolean isCenterAt(ISPosition2D pos) {
+			return pos != null && pos.getX() == cX && pos.getY() == cY;
+		}
+
+		void setRadius(short radius) {
+			this.radius = (short) (radius - this.minRadius + 1);
+		}
+	}
+
+	public final Path find(DijkstraContinuableRequest request) {
+		if (!isInBounds(request.cX, request.cY)) {
+			throw new InvalidStartPositionException("dijkstra center position is not in bounds!", request.cX, request.cY);
+		}
+
+		// TODO @Michael find out how much the radius must be bigger to be larger than the circle
+		MapCircle circle = new MapCircle(request.cX, request.cY, request.maxRadius * MAX_RADIUS_MULTIPLIER);
+
+		short radiusSteps = request.getRadiusSteps();
+		short radius = 1;
+
+		for (short deltaRadius = 0; deltaRadius < radiusSteps; deltaRadius++) {
+			radius = (short) ((deltaRadius + request.radius) % request.maxRadius + request.minRadius);
+			short x = request.cX, y = (short) (request.cY - radius);
+
+			for (byte direction = 0; direction < 6; direction++) {
+				byte dx = directionIncreaseX[direction];
+				byte dy = directionIncreaseY[direction];
+				for (short length = 0; length < radius; length++) {
+					x += dx;
+					y += dy;
+					if (circle.contains(x, y) && isInBounds(x, y)) {
+						map.setDijkstraSearched(x, y);
+						if (map.fitsSearchType(x, y, request.searchType, request.requester)) {
+							Path path = findPathTo(request.requester, x, y);
+							if (path != null) {
+								request.setRadius(radius);
+								return path;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		request.setRadius(radius);
+		return null;
 	}
 }
