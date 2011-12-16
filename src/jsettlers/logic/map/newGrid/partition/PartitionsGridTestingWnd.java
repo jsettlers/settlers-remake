8ee6a39daf549b2deaa2121aa1110cf71d01fd3e
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2011-12-16 20:35:25 +0000

    - made save work on android

--- src/jsettlers/logic/map/newGrid/partition/PartitionsGridTestingWnd.java
+++ src/jsettlers/logic/map/newGrid/partition/PartitionsGridTestingWnd.java
@@ -125,7 +125,7 @@ public class PartitionsGridTestingWnd extends JFrame {
 			}
 		};
 
-		partitionsGrid = new PartitionsGrid(WIDTH, HEIGHT, partitionableGrid, aStarMap);
+		partitionsGrid = new PartitionsGrid(WIDTH, HEIGHT, partitionableGrid);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 500);
