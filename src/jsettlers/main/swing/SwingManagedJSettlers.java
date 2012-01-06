b66ceff7f0e739df7e5a8b7abd7454c5b9441b03
Michael Zangl <michael>
2012-01-06 15:21:33 +0000

    Added Sound manager Added the ability to send user notifications to the UI.

--- src/jsettlers/main/swing/SwingManagedJSettlers.java
+++ src/jsettlers/main/swing/SwingManagedJSettlers.java
@@ -5,16 +5,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import go.graphics.swing.AreaContainer;
+import go.graphics.swing.sound.SwingSoundPlayer;
 import jsettlers.common.resources.ResourceManager;
+import jsettlers.graphics.ISettlersGameDisplay;
 import jsettlers.graphics.JOGLPanel;
 import jsettlers.graphics.JoglLibraryPathInitializer;
 import jsettlers.graphics.map.draw.ImageProvider;
+import jsettlers.graphics.sound.SoundManager;
 import jsettlers.main.ManagedJSettlers;
-import jsettlers.main.ManagedJSettlers.IGuiStarter;
 
 import javax.swing.JFrame;
 
-public class SwingManagedJSettlers implements IGuiStarter {
+public class SwingManagedJSettlers {
 
 	static { // sets the native library path for the system dependent jogl libs
 		JoglLibraryPathInitializer.initLibraryPath();
@@ -24,6 +26,11 @@ public class SwingManagedJSettlers implements IGuiStarter {
 		        "/home/michael/.wine/drive_c/BlueByte/S3AmazonenDemo/GFX"));
 		provider.addLookupPath(new File("D:/Games/Siedler3/GFX"));
 		provider.addLookupPath(new File("C:/Program Files/siedler 3/GFX"));
+	
+		SoundManager.addLookupPath(new File(
+		        "/home/michael/.wine/drive_c/BlueByte/S3AmazonenDemo/Snd"));
+		SoundManager.addLookupPath(new File("D:/Games/Siedler3/Snd"));
+		SoundManager.addLookupPath(new File("C:/Program Files/siedler 3/Snd"));
 	}
 
 	/**
@@ -37,14 +44,14 @@ public class SwingManagedJSettlers implements IGuiStarter {
 	public static void main(String[] args) {
 		ResourceManager.setProvider(new ResourceProvider());
 		ManagedJSettlers game = new ManagedJSettlers();
-
-		game.start(new SwingManagedJSettlers());
+		game.start(getGui());
 
 		// NetworkTimer.loadLogging("logs/2011_11_02-11_39_44.log");
 		// NetworkTimer.activateLogging("logs");
 	}
 
-	public void startGui(JOGLPanel content) {
+	private static ISettlersGameDisplay getGui() {
+		JOGLPanel content = new JOGLPanel(new SwingSoundPlayer());
 		JFrame jsettlersWnd = new JFrame("jsettlers");
 		AreaContainer panel = new AreaContainer(content.getArea());
 		panel.setPreferredSize(new Dimension(640, 480));
@@ -56,5 +63,6 @@ public class SwingManagedJSettlers implements IGuiStarter {
 		jsettlersWnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jsettlersWnd.setVisible(true);
 		jsettlersWnd.setLocationRelativeTo(null);
-	}
+		return content;
+    }
 }
