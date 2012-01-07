48037d23eb5aaf14e2f97cb01dc82751e13c0f67
Michael Zangl <michael>
2012-01-07 22:26:06 +0000

    Made sound work with swing/linux/openjdk

--- src/go/graphics/swing/sound/SwingSoundPlayer.java
+++ src/go/graphics/swing/sound/SwingSoundPlayer.java
@@ -1,26 +1,30 @@
 package go.graphics.swing.sound;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.RandomAccessFile;
+import go.graphics.sound.ForgettingQueue;
+import go.graphics.sound.ForgettingQueue.Sound;
+import go.graphics.sound.SoundPlayer;
+
 import java.util.ArrayList;
 
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioFormat.Encoding;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
-import go.graphics.sound.SoundPlayer;
-
 public class SwingSoundPlayer implements SoundPlayer {
-	private static final int BUFFER_SIZE = 4048;
-	ArrayList<Clip> clips = new ArrayList<Clip>();
+	private static final int BUFFER_SIZE = 4048 * 4;
+	private static final int SOUND_THREADS = 3;
+	ArrayList<byte[]> clips = new ArrayList<byte[]>();
+
+	ForgettingQueue<byte[]> queue = new ForgettingQueue<byte[]>();
 
 	public SwingSoundPlayer() {
-
+		ThreadGroup soundgroup = new ThreadGroup("soundplayer");
+		for (int i = 0; i < SOUND_THREADS; i++) {
+			new Thread(soundgroup, new SoundPlayerTask(), "soundplayer" + i)
+			        .start();
+		}
 	}
 
 	@Override
@@ -29,7 +33,8 @@ public class SwingSoundPlayer implements SoundPlayer {
 			return;
 		}
 		System.out.println("playing sound");
-		Clip clip = clips.get(sound);
+		byte[] clip = clips.get(sound);
+		queue.offer(clip, lvolume, rvolume);
 	}
 
 	@Override
@@ -38,32 +43,55 @@ public class SwingSoundPlayer implements SoundPlayer {
 			System.out.println("loading sound file ");
 			int index = clips.size();
 
-			AudioFormat format =
-			        new AudioFormat(Encoding.PCM_SIGNED, 44100, 16, 1, 2,
-			                44100, false);
-			
-			Line.Info info = new Line.Info(SourceDataLine.class);
-
-			SourceDataLine dataLine =
-			        (SourceDataLine) AudioSystem.getLine(info);
-			dataLine.open(format, BUFFER_SIZE);
-
-			
-			//Sound playing code
-			dataLine.start();
-
-			byte[] buffer = new byte[BUFFER_SIZE];
-			for (int i = 0; i < BUFFER_SIZE / 2 && i < data.length; i++) {
-				buffer[i] = (byte) data[i];
-				buffer[i + 1] = (byte) (data[i] >> 8);
+			byte[] buffer = new byte[data.length * 4];
+			for (int i = 0; i < data.length; i++) {
+				buffer[4 * i] = buffer[4 * i + 2] = (byte) data[i];
+				buffer[4 * i + 1] = buffer[4 * i + 3] = (byte) (data[i] >> 8);
 			}
-			dataLine.write(buffer, 0, buffer.length);
-			
+			clips.add(buffer);
 
 			return index;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return -1;
 		}
+	}
+
+	private class SoundPlayerTask implements Runnable {
+
+		@Override
+		public void run() {
+
+			AudioFormat format = new AudioFormat(22050, 16, 2, true, false);
+
+			Line.Info info = new Line.Info(SourceDataLine.class);
+
+			try {
+				SourceDataLine dataLine =
+				        (SourceDataLine) AudioSystem.getMixer(null).getLine(
+				                info);
+				dataLine.open(format, BUFFER_SIZE);
+
+				while (true) {
+					// Sound playing code
+					dataLine.start();
+
+					Sound<byte[]> sound = queue.take();
+					byte[] buffer = sound.getData();
+					dataLine.write(buffer, 0, buffer.length);
+
+					// stop playing
+					dataLine.drain();
+					dataLine.stop();
+				}
+
+			} catch (InterruptedException e) {
+				// exit
+
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
