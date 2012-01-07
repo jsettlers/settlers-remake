abdae67d9a2c9e22f20281fe7070eceb341916ec
Michael Zangl <michael>
2012-01-07 20:06:13 +0000

    Added background sound and sound for messages.

--- src/go/graphics/sound/SoundPlayer.java
+++ src/go/graphics/sound/SoundPlayer.java
@@ -1,12 +1,20 @@
 package go.graphics.sound;
 
-import java.io.File;
-
 public interface SoundPlayer {
+	/**
+	 * Starts to play a sound.
+	 * <p>
+	 * Blocks until the sound is played.
+	 * @param sound
+	 * @param lvolume
+	 * @param rvolume
+	 */
 	public void playSound(int sound, float lvolume, float rvolume);
 
 	/**
 	 * Loads a signed 16 bit little endian stram from the sound file.
+	 * <p>
+	 * This method will block until the sound is loaded.
 	 * @param sndfile The file to load
 	 * @param leftstart The start byte offset of left 
 	 * @param rightstart 
