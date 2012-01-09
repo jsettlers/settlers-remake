2b3806ab01bcb05684b2c359120d5154c24eb07e
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-01-09 15:52:16 +0000

    - added constant to enable / disable that fog of war shows other players

--- src/jsettlers/common/CommonConstants.java
+++ src/jsettlers/common/CommonConstants.java
@@ -5,6 +5,11 @@ public abstract class CommonConstants {
 	public static final int FOG_OF_WAR_EXPLORED = 50;
 
 	/**
+	 * if true, the user will be able to see other players people and buildings
+	 */
+	public static final boolean ENABLE_ALL_PLAYER_FOG_OF_WAR = false;
+
+	/**
 	 * if true, the user will be able to select other player's people and buildings.
 	 */
 	public static boolean ENABLE_ALL_PLAYER_SELECTION = true;
