5c588645d3a60c8e251419fa6e4ded3935f5bd20
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-01-09 15:40:21 +0000

    - added constant to CommonConstants to enable and disable selection of other players movables and buildings

--- src/jsettlers/common/CommonConstants.java
+++ src/jsettlers/common/CommonConstants.java
@@ -4,4 +4,8 @@ public abstract class CommonConstants {
 	public static final int FOG_OF_WAR_VISIBLE = 100;
 	public static final int FOG_OF_WAR_EXPLORED = 50;
 
+	/**
+	 * if true, the user will be able to select other player's people and buildings.
+	 */
+	public static boolean ENABLE_ALL_PLAYER_SELECTION = true;
 }
