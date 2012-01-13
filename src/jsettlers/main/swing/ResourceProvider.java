7ce9d01e210ed4ebca12f5b1e21074a9f45409de
Michael Zangl <michael>
2012-01-13 21:02:05 +0000

    Added map loader and the ability to select the map to load. Random maps don't work yet.

--- src/jsettlers/main/swing/ResourceProvider.java
+++ src/jsettlers/main/swing/ResourceProvider.java
@@ -11,23 +11,36 @@ import jsettlers.common.resources.IResourceProvider;
 
 public class ResourceProvider implements IResourceProvider {
 	private String path;
+	private File userPath;
 
 	public ResourceProvider() {
-		String currentParentFolder = new File(new File("").getAbsolutePath()).getParent().replace('\\', '/');
+		String currentParentFolder =
+		        new File(new File("").getAbsolutePath()).getParent().replace(
+		                '\\', '/');
 		path = currentParentFolder + "/jsettlers.common/resources/";
+		userPath = new File(System.getProperty("user.home"), ".jsettlers");
+		userPath.mkdirs();
 	}
 
 	@Override
 	public InputStream getFile(String name) throws IOException {
-		File file = new File(path + name);
+		File file = new File(userPath.getAbsolutePath() + "/" + name);
+		if (!file.exists()) {
+			file = new File(path + name);
+		}
 		return new FileInputStream(file);
 	}
 
 	@Override
 	public OutputStream writeFile(String name) throws IOException {
-		File file = new File(path + name);
+		File file = new File(userPath.getAbsolutePath() + name);
 		file.getParentFile().mkdirs();
 		return new FileOutputStream(file);
+	}
+
+	@Override
+	public File getSaveDirectory() {
+		return userPath;
 	}
 
 }
