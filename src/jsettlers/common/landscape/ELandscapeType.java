4a79faada653b2fbbfe24c805a8d0dd4960cd331
Michael Zangl <michael>
2012-01-17 19:29:39 +0000

    Improved minimap and background drawing: Fixed minimap colors Added height shades to minimap. Added paths to background fixed missing grass<->flattened image.

--- src/jsettlers/common/landscape/ELandscapeType.java
+++ src/jsettlers/common/landscape/ELandscapeType.java
@@ -4,29 +4,38 @@ import jsettlers.common.Color;
 
 public enum ELandscapeType {
 	// DO NOT sort, order is important!
-	GRASS(0, new Color(0x3f9b0b), false),
-	DRY_GRASS(1, new Color(0x5cac2d), false),
-	DESERT(3, new Color(0xccad60), false),
-	EARTH(2, new Color(0xa2653e), false),
-	MOUNTAIN(21, new Color(0x59656d), false),
+	GRASS(0, new Color(0x105910), false),
+	DRY_GRASS(1, new Color(0x105910), false),
+	DESERT(3, new Color(0x949200), false),
+	EARTH(2, new Color(0xa2653e), false), //TODO: color
+	MOUNTAIN(21, new Color(0x424142), false),
 	SNOW(24, new Color(0xd7fffe), false),
-	SAND(3, new Color(0xccad60), false),
-	FLATTENED(35, new Color(0xcfaf7b), false),
-	RIVER1(10, new Color(0x0e87cc), false),
-	RIVER2(10, new Color(0x0e87cc), false),
-	RIVER3(10, new Color(0x0e87cc), false),
-	RIVER4(10, new Color(0x0e87cc), false),
-	MOUNTAINBORDER(21, new Color(0x59656d), false),
-	MOUNTAINBORDEROUTER(21, new Color(0x59656d), false),
-	WATER1(17, new Color(0x0e87cc), true),
-	WATER2(16, new Color(0x0e87cc), true),
-	WATER3(15, new Color(0x0e87cc), true),
-	WATER4(14, new Color(0x0e87cc), true),
-	WATER5(13, new Color(0x0e87cc), true),
-	WATER6(12, new Color(0x0e87cc), true),
-	WATER7(11, new Color(0x0e87cc), true),
-	WATER8(10, new Color(0x0e87cc), true),
-	MOOR(10, new Color(0x0e87cc), false), ;
+	SAND(3, new Color(0x949200), false),
+	/**
+	 * Flattened grass (for buildings, paths, ...).
+	 * Must behave exactly like normal grass does!
+	 */
+	FLATTENED(35, new Color(0x105910), false),
+	RIVER1(10, new Color(0x000073), false), 
+	RIVER2(10, new Color(0x000073), false), 
+	RIVER3(10, new Color(0x000073), false), 
+	RIVER4(10, new Color(0x000073), false), 
+	MOUNTAINBORDER(21, new Color(0x424142), false),
+	MOUNTAINBORDEROUTER(21, new Color(0x105910), false), //TODO: color
+	WATER1(17, new Color(0x000073), true),
+	WATER2(16, new Color(0x000073), true),
+	WATER3(15, new Color(0x000073), true),
+	WATER4(14, new Color(0x000073), true),
+	WATER5(13, new Color(0x000073), true),
+	WATER6(12, new Color(0x000073), true),
+	WATER7(11, new Color(0x000073), true),
+	WATER8(10, new Color(0x000073), true),
+	MOOR(7, new Color(0x0e87cc), false), //TODO: color
+	MOORINNER(7, new Color(0x0e87cc), false), //TODO: color
+	MOORBORDER(9, new Color(0x0e87cc), false), //TODO: color
+	FLATTENED_DESERT(217, new Color(0x949200), false),
+	SHARP_FLATTENED_DESERT(217, new Color(0x949200), false),
+	GRAVEL(230, new Color(0x00), false), ; //TODO: color
 
 	private final int image;
 	private final Color color;
@@ -62,4 +71,8 @@ public enum ELandscapeType {
 	public boolean isWater() {
 		return isWater;
 	}
+
+	public boolean isGrass() {
+	    return this == GRASS || this == FLATTENED;
+    }
 }
