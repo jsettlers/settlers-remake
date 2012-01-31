bb4ac87997d4e730bb8551ad142be80a42e33ea0
Michael Zangl <michael>
2012-01-31 15:28:13 +0000

    Added network start ability for Android

---
+++ src/jsettlers/mapcreator/data/MapDataHasher.java
@@ -0,0 +1,22 @@
+package jsettlers.mapcreator.data;
+
+import jsettlers.common.map.IMapData;
+
+/**
+ * 
+ * @author michael
+ *
+ */
+public class MapDataHasher {
+	public static long computeHash(IMapData data) {
+		long hash = 0;
+		
+		for (int x = 0; x < data.getWidth(); x++) {
+			for (int y = 0; y < data.getHeight(); y++) {
+				
+			}
+		}
+		
+		return 0;
+	}
+}
