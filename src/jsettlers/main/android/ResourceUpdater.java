b48bc90a793fa18fe85708113a55c44af402b153
Michael Zangl <michael>
2012-02-02 17:56:40 +0000

    Changed resource updater to save the last revision the server had.

--- src/jsettlers/main/android/ResourceUpdater.java
+++ src/jsettlers/main/android/ResourceUpdater.java
@@ -1,8 +1,9 @@
 package jsettlers.main.android;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +13,6 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
-import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -34,7 +34,8 @@ import android.content.res.Resources;
 public class ResourceUpdater implements Runnable {
 
 	private static final String RESOURCE_PREFIX = "";
-	private static final String SERVER_ROOT = "https://michael2402.homeip.net/jsettlers/";
+	private static final String SERVER_ROOT =
+	        "https://michael2402.homeip.net/jsettlers/";
 	private final Resources resources;
 	private final File destdir;
 
@@ -54,68 +55,80 @@ public class ResourceUpdater implements Runnable {
 			File versionfile = new File(destdir, "version");
 			String myrev = getMyVersion(versionfile);
 
-			if (serverrev != null && !myrev.equals(serverrev)) {
+			if (serverrev != null && !serverrev.equals(myrev)) {
 				// Download the file
 				System.out.println("resourceupdater: updating");
 				updateFiles(httpClient);
+
+				FileOutputStream fileOut = new FileOutputStream(versionfile);
+				DataOutputStream out = new DataOutputStream(fileOut);
+				out.writeUTF(serverrev);
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		setUpdating(false);
-
 	}
 
-	private void updateFiles(DefaultHttpClient httpClient) throws IOException, ClientProtocolException {
+	private void updateFiles(DefaultHttpClient httpClient) throws IOException,
+	        ClientProtocolException {
 		final String url = SERVER_ROOT + "resources.zip";
 		HttpGet httpRequest = new HttpGet(url);
 		HttpResponse response = httpClient.execute(httpRequest);
-		ZipInputStream inputStream = new ZipInputStream(response.getEntity().getContent());
+		ZipInputStream inputStream =
+		        new ZipInputStream(response.getEntity().getContent());
 		setUpdating(true);
 
-		int files = 0;
+		try {
 
-		byte[] buffer = new byte[1024];
+			int files = 0;
 
-		ZipEntry entry;
-		while ((entry = inputStream.getNextEntry()) != null) {
-			String name = entry.getName();
-			if (name.startsWith(RESOURCE_PREFIX)) {
-				String outfilename = destdir.getAbsolutePath() + "/" + name.substring(RESOURCE_PREFIX.length());
-				File outfile = new File(outfilename);
-				if (entry.isDirectory()) {
-					if (outfile.exists() && !outfile.isDirectory()) {
-						outfile.delete();
-					}
-					if (!outfile.isDirectory()) {
-						outfile.mkdirs();
-					}
-				} else {
-					File tmpFile = new File(outfilename + ".tmp");
-					tmpFile.getParentFile().mkdirs();
-					tmpFile.deleteOnExit(); // <- if something fails
-					FileOutputStream out = new FileOutputStream(tmpFile);
+			byte[] buffer = new byte[1024];
 
-					while (true) {
-						int len = inputStream.read(buffer);
-						if (len <= 0) {
-							break;
+			ZipEntry entry;
+			while ((entry = inputStream.getNextEntry()) != null) {
+				String name = entry.getName();
+				if (name.startsWith(RESOURCE_PREFIX)) {
+					String outfilename =
+					        destdir.getAbsolutePath() + "/"
+					                + name.substring(RESOURCE_PREFIX.length());
+					File outfile = new File(outfilename);
+					if (entry.isDirectory()) {
+						if (outfile.exists() && !outfile.isDirectory()) {
+							outfile.delete();
 						}
-						out.write(buffer, 0, len);
+						if (!outfile.isDirectory()) {
+							outfile.mkdirs();
+						}
+					} else {
+						File tmpFile = new File(outfilename + ".tmp");
+						tmpFile.getParentFile().mkdirs();
+						tmpFile.deleteOnExit(); // <- if something fails
+						FileOutputStream out = new FileOutputStream(tmpFile);
+
+						while (true) {
+							int len = inputStream.read(buffer);
+							if (len <= 0) {
+								break;
+							}
+							out.write(buffer, 0, len);
+						}
+						out.close();
+						tmpFile.renameTo(outfile);
+						files++;
 					}
-					out.close();
-					tmpFile.renameTo(outfile);
-					files++;
 				}
 			}
+			System.out.println("Updated " + files + " files");
+		} catch (Throwable t) {
+			t.printStackTrace();
 		}
-		System.out.println("Updated " + files + " files");
-
 		setUpdating(false);
 	}
 
-	private static String getMyVersion(File versionfile) throws FileNotFoundException {
+	private static String getMyVersion(File versionfile)
+	        throws IOException {
 		if (versionfile.exists()) {
 			return getString(new FileInputStream(versionfile));
 		} else {
@@ -123,7 +136,8 @@ public class ResourceUpdater implements Runnable {
 		}
 	}
 
-	private static String loadRevision(DefaultHttpClient httpClient) throws IOException, ClientProtocolException {
+	private static String loadRevision(DefaultHttpClient httpClient)
+	        throws IOException, ClientProtocolException {
 		final String url = SERVER_ROOT + "revision.txt";
 		HttpGet httpRequest = new HttpGet(url);
 		HttpResponse response = httpClient.execute(httpRequest);
@@ -131,13 +145,15 @@ public class ResourceUpdater implements Runnable {
 		return getString(inputStream);
 	}
 
-	private static String getString(InputStream inputStream) {
-		return new Scanner(inputStream).useDelimiter("\\A").next();
+	private static String getString(InputStream inputStream) throws IOException {
+		return new DataInputStream(inputStream).readUTF();
 	}
 
-	private DefaultHttpClient createClient() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException,
-			KeyManagementException, UnrecoverableKeyException {
-		HostnameVerifier hostnameVerifier = SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+	private DefaultHttpClient createClient() throws KeyStoreException,
+	        NoSuchAlgorithmException, CertificateException, IOException,
+	        KeyManagementException, UnrecoverableKeyException {
+		HostnameVerifier hostnameVerifier =
+		        SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
 
 		DefaultHttpClient client = new DefaultHttpClient();
 
@@ -149,10 +165,13 @@ public class ResourceUpdater implements Runnable {
 		SSLSocketFactory socketFactory = new SSLSocketFactory(truststore);
 
 		SchemeRegistry registry = new SchemeRegistry();
-		socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
+		socketFactory
+		        .setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
 		registry.register(new Scheme("https", socketFactory, 443));
-		SingleClientConnManager mgr = new SingleClientConnManager(client.getParams(), registry);
-		DefaultHttpClient httpClient = new DefaultHttpClient(mgr, client.getParams());
+		SingleClientConnManager mgr =
+		        new SingleClientConnManager(client.getParams(), registry);
+		DefaultHttpClient httpClient =
+		        new DefaultHttpClient(mgr, client.getParams());
 
 		// Set verifier
 		HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
@@ -163,7 +182,8 @@ public class ResourceUpdater implements Runnable {
 		return isUpdating;
 	}
 
-	public synchronized void waitUntilUpdateFinished() throws InterruptedException {
+	public synchronized void waitUntilUpdateFinished()
+	        throws InterruptedException {
 		while (isUpdating) {
 			this.wait();
 		}
