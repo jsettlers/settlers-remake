4e21adcb1df28437f28176c6851ce04636311323
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-02-02 18:36:14 +0000

    - changed LandscapeGrid to use byte arrays instead of enum arrays

--- src/jsettlers/common/landscape/ELandscapeType.java
+++ src/jsettlers/common/landscape/ELandscapeType.java
@@ -7,21 +7,20 @@ public enum ELandscapeType {
 	GRASS(0, new Color(0x105910), false),
 	DRY_GRASS(1, new Color(0x105910), false),
 	DESERT(3, new Color(0x949200), false),
-	EARTH(2, new Color(0xa2653e), false), //TODO: color
+	EARTH(2, new Color(0xa2653e), false), // TODO: color
 	MOUNTAIN(21, new Color(0x424142), false),
 	SNOW(24, new Color(0xd7fffe), false),
 	SAND(3, new Color(0x949200), false),
 	/**
-	 * Flattened grass (for buildings, paths, ...).
-	 * Must behave exactly like normal grass does!
+	 * Flattened grass (for buildings, paths, ...). Must behave exactly like normal grass does!
 	 */
 	FLATTENED(35, new Color(0x105910), false),
-	RIVER1(10, new Color(0x000073), false), 
-	RIVER2(10, new Color(0x000073), false), 
-	RIVER3(10, new Color(0x000073), false), 
-	RIVER4(10, new Color(0x000073), false), 
+	RIVER1(10, new Color(0x000073), false),
+	RIVER2(10, new Color(0x000073), false),
+	RIVER3(10, new Color(0x000073), false),
+	RIVER4(10, new Color(0x000073), false),
 	MOUNTAINBORDER(21, new Color(0x424142), false),
-	MOUNTAINBORDEROUTER(21, new Color(0x105910), false), //TODO: color
+	MOUNTAINBORDEROUTER(21, new Color(0x105910), false), // TODO: color
 	WATER1(17, new Color(0x000073), true),
 	WATER2(16, new Color(0x000073), true),
 	WATER3(15, new Color(0x000073), true),
@@ -30,21 +29,25 @@ public enum ELandscapeType {
 	WATER6(12, new Color(0x000073), true),
 	WATER7(11, new Color(0x000073), true),
 	WATER8(10, new Color(0x000073), true),
-	MOOR(7, new Color(0x0e87cc), false), //TODO: color
-	MOORINNER(7, new Color(0x0e87cc), false), //TODO: color
-	MOORBORDER(9, new Color(0x0e87cc), false), //TODO: color
+	MOOR(7, new Color(0x0e87cc), false), // TODO: color
+	MOORINNER(7, new Color(0x0e87cc), false), // TODO: color
+	MOORBORDER(9, new Color(0x0e87cc), false), // TODO: color
 	FLATTENED_DESERT(217, new Color(0x949200), false),
 	SHARP_FLATTENED_DESERT(217, new Color(0x949200), false),
-	GRAVEL(230, new Color(0x00), false), ; //TODO: color
+	GRAVEL(230, new Color(0x00), false); // TODO: color
+
+	public static final ELandscapeType[] values = ELandscapeType.values();
 
 	private final int image;
 	private final Color color;
 	private final boolean isWater;
+	public final byte ordinal;
 
 	ELandscapeType(int image, Color color, boolean isWater) {
 		this.image = image;
 		this.color = color;
 		this.isWater = isWater;
+		this.ordinal = (byte) super.ordinal();
 	}
 
 	public int getImageNumber() {
@@ -61,8 +64,7 @@ public enum ELandscapeType {
 	}
 
 	/**
-	 * Checks if this landscape type is water (not river, just water that ships
-	 * can swim on.).
+	 * Checks if this landscape type is water (not river, just water that ships can swim on.).
 	 * <p>
 	 * To check for unwalkable land, also test if it is MOOR or SNOW
 	 * 
@@ -73,6 +75,6 @@ public enum ELandscapeType {
 	}
 
 	public boolean isGrass() {
-	    return this == GRASS || this == FLATTENED;
-    }
+		return this == GRASS || this == FLATTENED;
+	}
 }
