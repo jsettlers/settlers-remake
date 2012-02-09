c1786f6cfbed7833114d17833801a77e50e54ae5
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-02-09 15:20:48 +0000

    - moved Color class to common and removed usage from the go projects

--- src/jsettlers/common/Color.java
+++ src/jsettlers/common/Color.java
@@ -1,6 +1,6 @@
 package jsettlers.common;
 
-public class Color extends go.graphics.Color {
+public final class Color {
 	public static final Color BLACK = new Color(0, 0, 0, 1);
 	public static final Color WHITE = new Color(1, 1, 1, 1);
 	public static final Color TRANSPARENT = new Color(1, 1, 1, 0);
@@ -8,23 +8,40 @@ public class Color extends go.graphics.Color {
 	public static final Color BLUE = new Color(0, 0, 1, 1);
 	public static final Color GREEN = new Color(0, 1, 0, 1);
 
+	public final float blue;
+	public final float red;
+	public final float green;
+	public final float alpha;
+
+	public Color(int rgbhex) {
+		this(((rgbhex >> 16) & 0xff) / 255f, ((rgbhex >> 8) & 0xff) / 255f, ((rgbhex >> 0) & 0xff) / 255f, 1f);
+	}
+
 	public Color(float red, float green, float blue, float alpha) {
-		super(red, green, blue, alpha);
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+		this.alpha = alpha;
 	}
 
-	public Color(int i) {
-		super(i);
+	public final float getAlpha() {
+		return alpha;
 	}
 
-	public Color multiply(float f) {
-		float alpha = getAlpha();
-		if (f == 1 && alpha == 1) {
-			return this;
-		} else if (f == 0 && alpha == 1) {
-			return Color.BLACK;
-		} else {
-		return new Color(Math.min(getRed() * f, 1), Math.min(getGreen() * f, 1), Math.min(
-		        getBlue() * f, 1), alpha);
-		}
+	public final float getBlue() {
+		return blue;
+	}
+
+	public final float getGreen() {
+		return green;
+	}
+
+	public final float getRed() {
+		return red;
+	}
+
+	public final int getRGB() {
+		return ((int) (alpha * 255) & 0xff) << 24 | ((int) (red * 255) & 0xff) << 16 | ((int) (green * 255) & 0xff) << 8
+				| ((int) (blue * 255) & 0xff);
 	}
 }
