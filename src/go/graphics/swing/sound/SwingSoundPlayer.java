7aa1e86b942e3245fd012790c47ccd48fbab2079
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-02-09 18:35:23 +0000

    - changed soundplayers to load the sound data directly when it's played instead of caching it => reduced heap by about 10MB

--- src/go/graphics/swing/sound/SwingSoundPlayer.java
+++ src/go/graphics/swing/sound/SwingSoundPlayer.java
@@ -2,9 +2,10 @@ package go.graphics.swing.sound;
 
 import go.graphics.sound.ForgettingQueue;
 import go.graphics.sound.ForgettingQueue.Sound;
+import go.graphics.sound.ISoundDataRetriever;
 import go.graphics.sound.SoundPlayer;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -15,45 +16,34 @@ import javax.sound.sampled.SourceDataLine;
 public class SwingSoundPlayer implements SoundPlayer {
 	private static final int BUFFER_SIZE = 4048 * 4;
 	private static final int SOUND_THREADS = 3;
-	ArrayList<byte[]> clips = new ArrayList<byte[]>();
 
-	ForgettingQueue<byte[]> queue = new ForgettingQueue<byte[]>();
+	ForgettingQueue<Integer> queue = new ForgettingQueue<Integer>();
+	private ISoundDataRetriever soundDataRetriever;
 
 	public SwingSoundPlayer() {
 		ThreadGroup soundgroup = new ThreadGroup("soundplayer");
 		for (int i = 0; i < SOUND_THREADS; i++) {
-			new Thread(soundgroup, new SoundPlayerTask(), "soundplayer" + i)
-			        .start();
+			new Thread(soundgroup, new SoundPlayerTask(), "soundplayer" + i).start();
 		}
 	}
 
 	@Override
-	public void playSound(int sound, float lvolume, float rvolume) {
-		if (sound < 0 || sound > clips.size()) {
-			return;
-		}
-		System.out.println("playing sound");
-		byte[] clip = clips.get(sound);
-		queue.offer(clip, lvolume, rvolume);
+	public void playSound(int soundStart, float lvolume, float rvolume) {
+		queue.offer(soundStart, lvolume, rvolume);
 	}
 
-	@Override
-	public int load(short[] data) {
+	public byte[] transformData(short[] data) {
 		try {
-			System.out.println("loading sound file ");
-			int index = clips.size();
-
 			byte[] buffer = new byte[data.length * 4];
 			for (int i = 0; i < data.length; i++) {
 				buffer[4 * i] = buffer[4 * i + 2] = (byte) data[i];
 				buffer[4 * i + 1] = buffer[4 * i + 3] = (byte) (data[i] >> 8);
 			}
-			clips.add(buffer);
 
-			return index;
+			return buffer;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return -1;
+			return null;
 		}
 	}
 
@@ -61,37 +51,45 @@ public class SwingSoundPlayer implements SoundPlayer {
 
 		@Override
 		public void run() {
-
 			AudioFormat format = new AudioFormat(22050, 16, 2, true, false);
 
 			Line.Info info = new Line.Info(SourceDataLine.class);
 
 			try {
-				SourceDataLine dataLine =
-				        (SourceDataLine) AudioSystem.getMixer(null).getLine(
-				                info);
+				SourceDataLine dataLine = (SourceDataLine) AudioSystem.getMixer(null).getLine(info);
 				dataLine.open(format, BUFFER_SIZE);
 
 				while (true) {
-					// Sound playing code
-					dataLine.start();
+					try {
+						// start sound playing
+						dataLine.start();
 
-					Sound<byte[]> sound = queue.take();
-					byte[] buffer = sound.getData();
-					dataLine.write(buffer, 0, buffer.length);
+						Sound<Integer> sound = queue.take();
+						long start = System.currentTimeMillis();
+						byte[] buffer = transformData(soundDataRetriever.getSoundData(sound.getStart()));
+						System.out.println("loading sound took: " + (System.currentTimeMillis() - start) + " ms");
 
-					// stop playing
-					dataLine.drain();
-					dataLine.stop();
+						dataLine.write(buffer, 0, buffer.length);
+
+						// stop playing
+						dataLine.drain();
+						dataLine.stop();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 
 			} catch (InterruptedException e) {
 				// exit
-
 			} catch (LineUnavailableException e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
+
+	@Override
+	public void setSoundDataRetriever(ISoundDataRetriever soundDataRetriever) {
+		this.soundDataRetriever = soundDataRetriever;
+	}
+
 }
