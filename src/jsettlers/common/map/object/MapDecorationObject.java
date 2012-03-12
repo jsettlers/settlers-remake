cf10c156a82f8cf718ae15b258b603653df502a8
Michael Zangl <michael>
2012-03-12 12:44:31 +0000

    Added map decoration object to allow decorations to be placed on created maps.

---
+++ src/jsettlers/common/map/object/MapDecorationObject.java
@@ -0,0 +1,14 @@
+package jsettlers.common.map.object;
+
+import jsettlers.common.mapobject.EMapObjectType;
+
+public class MapDecorationObject implements MapObject {
+	private final EMapObjectType type;
+	public MapDecorationObject(EMapObjectType type) {
+		this.type = type;
+	}
+	
+	public EMapObjectType getType() {
+		return type;
+	}
+}
