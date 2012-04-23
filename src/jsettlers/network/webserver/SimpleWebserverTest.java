3969c350d5285e614541f571fa6c3a9aa6e33cfe
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-04-23 13:03:32 +0000

    - added possibility to request data from server rest api

--- src/jsettlers/network/webserver/SimpleWebserverTest.java
+++ src/jsettlers/network/webserver/SimpleWebserverTest.java
@@ -7,6 +7,7 @@ public final class SimpleWebserverTest {
 	 */
 	public static void main(String[] args) {
 		SimpleWebServer webserver = new SimpleWebServer(10001);
+		webserver.setDaemon(false);
 		webserver.start();
 
 		webserver.addHandler(new TestHandler());
