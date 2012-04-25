9f6e500a8b6a493b012c7c5cef3dd1e10530ac9c
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-04-25 10:16:13 +0000

    - smaller bugfixes and refactorings

--- src/jsettlers/common/selectable/ESelectionType.java
+++ src/jsettlers/common/selectable/ESelectionType.java
@@ -8,14 +8,16 @@ package jsettlers.common.selectable;
  * 
  */
 public enum ESelectionType {
-	PEOPLE,
-	BUILDING,
-	SPECIALISTS,
-	SOLDIERS;
+	PEOPLE(Integer.MAX_VALUE),
+	BUILDING(1),
+	SPECIALISTS(Integer.MAX_VALUE),
+	SOLDIERS(Integer.MAX_VALUE);
 
 	public final int priority;
+	public final int maxSelected;
 
-	ESelectionType() {
-		priority = super.ordinal();
+	ESelectionType(int maxSelected) {
+		this.maxSelected = maxSelected;
+		this.priority = super.ordinal();
 	}
 }
