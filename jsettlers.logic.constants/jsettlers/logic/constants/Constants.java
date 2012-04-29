0fa97da966f5790784abf2eb8817cb2a52dc003e
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-04-29 08:53:12 +0000

    - small refactorings - removed bin from jsettlers.algorithms - speeded up construction of buildings a bit

--- jsettlers.logic.constants/jsettlers/logic/constants/Constants.java
+++ jsettlers.logic.constants/jsettlers/logic/constants/Constants.java
@@ -47,7 +47,7 @@ public final class Constants {
 
 	public static final short PIKEMAN_SEARCH_RADIUS = 12;
 
-	public static final byte BRICKLAYER_ACTIONS_PER_MATERIAL = 15;
+	public static final byte BRICKLAYER_ACTIONS_PER_MATERIAL = 12;
 
 	public static final int TILES_PER_DIGGER = 15;
 
