e2f4179ae15d493158e1bb4a60f482972950c0f8
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-07-19 18:20:39 +0000

    - implemented BlockedPartitioner

---
+++ src/jsettlers/common/map/EWalkableTypes.java
@@ -0,0 +1,12 @@
+package jsettlers.common.map;
+
+/**
+ * This enum defines the possible definitions of walkable areas.
+ * 
+ * @author Andreas Eberle
+ * 
+ */
+public enum EWalkableTypes {
+	SETTLER,
+	PLAYER_SETTLER
+}
