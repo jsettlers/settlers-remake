acf6318c1d48aa66b916f396474f54c4fdab3142
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-09-10 18:35:18 +0000

    fixed issue #0040 (if building worker is killed, the building doesn't get a new one)

--- src/jsettlers/common/material/EMaterialType.java
+++ src/jsettlers/common/material/EMaterialType.java
@@ -6,52 +6,54 @@ package jsettlers.common.material;
  * @author Andreas Eberle
  */
 public enum EMaterialType {
-	NO_MATERIAL((short) 0, (short) 0, (short) 0),
+	NO_MATERIAL((short) 0, (short) 0, (short) 0, false),
 
-	AXE((short) 46, (short) 3, (short) 153),
-	BOW((short) 63, (short) 14, (short) 114),
-	BLADE((short) 55, (short) 3, (short) 138),
-	BREAD((short) 49, (short) 3, (short) 186),
-	COAL((short) 34, (short) 3, (short) 144),
-	CROP((short) 50, (short) 3, (short) 180),
-	FISH((short) 47, (short) 3, (short) 189),
-	FISHINGROD((short) 66, (short) 3, (short) 141),
-	FLOUR((short) 48, (short) 3, (short) 183),
-	GOLD((short) 36, (short) 3, (short) 135),
-	GOLDORE((short) 35, (short) 3, (short) 150),
-	HAMMER((short) 51, (short) 3, (short) 126),
-	IRON((short) 42, (short) 3, (short) 132),
-	IRONORE((short) 39, (short) 3, (short) 147),
-	MEAT((short) 52, (short) 3, (short) 162),
-	PICK((short) 53, (short) 3, (short) 129),
-	PIG((short) 73, (short) 3, (short) 159),
-	PLANK((short) 33, (short) 3, (short) 168),
-	SAW((short) 54, (short) 3, (short) 177),
-	SCYTHE((short) 56, (short) 3, (short) 165),
-	SPEAR((short) 60, (short) 14, (short) 117),
-	STONE((short) 43, (short) 3, (short) 174),
-	SWORD((short) 59, (short) 14, (short) 111),
-	TRUNK((short) 41, (short) 3, (short) 171),
-	WATER((short) 77, (short) 3, (short) 156),
-	WINE((short) 69, (short) 14, (short) 123),
+	AXE((short) 46, (short) 3, (short) 153, true),
+	BOW((short) 63, (short) 14, (short) 114, true),
+	BLADE((short) 55, (short) 3, (short) 138, true),
+	BREAD((short) 49, (short) 3, (short) 186, true),
+	COAL((short) 34, (short) 3, (short) 144, true),
+	CROP((short) 50, (short) 3, (short) 180, true),
+	FISH((short) 47, (short) 3, (short) 189, true),
+	FISHINGROD((short) 66, (short) 3, (short) 141, true),
+	FLOUR((short) 48, (short) 3, (short) 183, true),
+	GOLD((short) 36, (short) 3, (short) 135, true),
+	GOLDORE((short) 35, (short) 3, (short) 150, true),
+	HAMMER((short) 51, (short) 3, (short) 126, true),
+	IRON((short) 42, (short) 3, (short) 132, true),
+	IRONORE((short) 39, (short) 3, (short) 147, true),
+	MEAT((short) 52, (short) 3, (short) 162, true),
+	PICK((short) 53, (short) 3, (short) 129, true),
+	PIG((short) 73, (short) 3, (short) 159, true),
+	PLANK((short) 33, (short) 3, (short) 168, true),
+	SAW((short) 54, (short) 3, (short) 177, true),
+	SCYTHE((short) 56, (short) 3, (short) 165, true),
+	SPEAR((short) 60, (short) 14, (short) 117, true),
+	STONE((short) 43, (short) 3, (short) 174, true),
+	SWORD((short) 59, (short) 14, (short) 111, true),
+	TRUNK((short) 41, (short) 3, (short) 171, true),
+	WATER((short) 77, (short) 3, (short) 156, true),
+	WINE((short) 69, (short) 14, (short) 123, true),
 
-	WHITE_BREAD((short) 0, (short) 0, (short) 0),
-	BASKET((short) 0, (short) 0, (short) 0),
-	TREE((short) 0, (short) 0, (short) 0),
-	PLANT((short) 0, (short) 0, (short) 0);
+	WHITE_BREAD((short) 0, (short) 0, (short) 0, false),
+	BASKET((short) 0, (short) 0, (short) 0, false),
+	TREE((short) 0, (short) 0, (short) 0, false),
+	PLANT((short) 0, (short) 0, (short) 0, false), ;
 
 	public static final EMaterialType[] values = EMaterialType.values();
 
 	private final short stackIndex;
 	private final short guiBase;
 	private final short guiFile;
+	private final boolean droppable;
 	public final byte ordinal;
 
-	EMaterialType(short stackIndex, short guiFile, short guiBase) {
+	EMaterialType(short stackIndex, short guiFile, short guiBase, boolean droppable) {
 		this.stackIndex = stackIndex;
 		this.guiFile = guiFile;
 		this.guiBase = guiBase;
 		this.ordinal = (byte) super.ordinal();
+		this.droppable = droppable;
 	}
 
 	/**
@@ -75,6 +77,10 @@ public enum EMaterialType {
 
 	public short getGuiFile() {
 		return guiFile;
+	}
+
+	public boolean isDroppable() {
+		return droppable;
 	}
 
 }
