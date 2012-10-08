76bf12b605fcfc8b3cb270cc2589dd2ad1ff168b
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-10-08 10:55:52 +0000

    moved some JUnit tests to jsettlers.tests and removed the JUnit dependency from the other projects

---
+++ src/jsettlers/common/position/RelativePointTests.java
@@ -0,0 +1,22 @@
+package jsettlers.common.position;
+
+import static org.junit.Assert.assertEquals;
+import jsettlers.common.position.RelativePoint;
+import jsettlers.common.position.ShortPoint2D;
+
+import org.junit.Test;
+
+public class RelativePointTests {
+
+	@Test
+	public void testCalculatePoint() {
+		ShortPoint2D p1 = new ShortPoint2D((short) 1, (short) 2);
+		ShortPoint2D p2 = new ShortPoint2D((short) 5, (short) 6);
+
+		RelativePoint expected = new RelativePoint((short) 4, (short) 4);
+		assertEquals(expected, RelativePoint.getRelativePoint(p1, p2));
+
+		assertEquals(p2, expected.calculatePoint(p1));
+	}
+
+}
