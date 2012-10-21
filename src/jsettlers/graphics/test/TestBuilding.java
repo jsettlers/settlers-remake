42cf7ec9b8c341e47e94fbddcb096787891d9b9a
Michael Zangl <michael>
2012-10-21 12:44:06 +0000

    Added new building/material/movable statistics interface.

--- src/jsettlers/graphics/test/TestBuilding.java
+++ src/jsettlers/graphics/test/TestBuilding.java
@@ -1,7 +1,11 @@
 package jsettlers.graphics.test;
 
+import java.util.Collections;
+import java.util.List;
+
 import jsettlers.common.buildings.EBuildingType;
 import jsettlers.common.buildings.IBuilding;
+import jsettlers.common.buildings.IBuildingMaterial;
 import jsettlers.common.mapobject.EMapObjectType;
 import jsettlers.common.mapobject.IMapObject;
 import jsettlers.common.position.ShortPoint2D;
@@ -83,4 +87,8 @@ public class TestBuilding implements IBuilding {
 		return ESelectionType.BUILDING;
 	}
 
+	@Override
+    public List<IBuildingMaterial> getMaterials() {
+	    return Collections.emptyList();
+    }
 }
