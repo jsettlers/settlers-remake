d003844d74548d0f13df578109dee4a2e735f15e
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-10-31 12:58:07 +0000

    - map editor now calculates the blocked partitions and stores them in the map files

--- src/jsettlers/common/landscape/ELandscapeType.java
+++ src/jsettlers/common/landscape/ELandscapeType.java
@@ -4,53 +4,55 @@ import jsettlers.common.Color;
 
 public enum ELandscapeType {
 	// DO NOT sort, order is important!
-	GRASS(0, new Color(0xff105910), false),
-	DRY_GRASS(1, new Color(0xff105910), false),
-	DESERT(3, new Color(0xff949200), false),
-	EARTH(2, new Color(0xffa2653e), false), // TODO: color
-	MOUNTAIN(21, new Color(0xff424142), false),
-	SNOW(24, new Color(0xffd7fffe), false),
-	SAND(3, new Color(0xff949200), false),
+	GRASS(0, new Color(0xff105910), false, false),
+	DRY_GRASS(1, new Color(0xff105910), false, false),
+	DESERT(3, new Color(0xff949200), false, false),
+	EARTH(2, new Color(0xffa2653e), false, false), // TODO: color
+	MOUNTAIN(21, new Color(0xff424142), false, false),
+	SNOW(24, new Color(0xffd7fffe), false, true),
+	SAND(3, new Color(0xff949200), false, false),
 	/**
 	 * Flattened grass (for buildings, paths, ...). Must behave exactly like normal grass does!
 	 */
-	FLATTENED(35, new Color(0xff105910), false),
-	RIVER1(10, new Color(0xff000073), false),
-	RIVER2(10, new Color(0xff000073), false),
-	RIVER3(10, new Color(0xff000073), false),
-	RIVER4(10, new Color(0xff000073), false),
-	MOUNTAINBORDER(21, new Color(0xff424142), false),
-	MOUNTAINBORDEROUTER(21, new Color(0xff105910), false), // TODO: color
-	WATER1(17, new Color(0xff000073), true),
-	WATER2(16, new Color(0xff000073), true),
-	WATER3(15, new Color(0xff000073), true),
-	WATER4(14, new Color(0xff000073), true),
-	WATER5(13, new Color(0xff000073), true),
-	WATER6(12, new Color(0xff000073), true),
-	WATER7(11, new Color(0xff000073), true),
-	WATER8(10, new Color(0xff000073), true),
-	MOOR(7, new Color(0xff0e87cc), false), // TODO: color
-	MOORINNER(7, new Color(0xff0e87cc), false), // TODO: color
-	MOORBORDER(9, new Color(0xff0e87cc), false), // TODO: color
-	FLATTENED_DESERT(217, new Color(0xff949200), false),
-	SHARP_FLATTENED_DESERT(217, new Color(0xff949200), false),
-	GRAVEL(230, new Color(0xff000000), false); // TODO: color
+	FLATTENED(35, new Color(0xff105910), false, false),
+	RIVER1(10, new Color(0xff000073), false, false),
+	RIVER2(10, new Color(0xff000073), false, false),
+	RIVER3(10, new Color(0xff000073), false, false),
+	RIVER4(10, new Color(0xff000073), false, false),
+	MOUNTAINBORDER(21, new Color(0xff424142), false, false),
+	MOUNTAINBORDEROUTER(21, new Color(0xff105910), false, false), // TODO: color
+	WATER1(17, new Color(0xff000073), true, true),
+	WATER2(16, new Color(0xff000073), true, true),
+	WATER3(15, new Color(0xff000073), true, true),
+	WATER4(14, new Color(0xff000073), true, true),
+	WATER5(13, new Color(0xff000073), true, true),
+	WATER6(12, new Color(0xff000073), true, true),
+	WATER7(11, new Color(0xff000073), true, true),
+	WATER8(10, new Color(0xff000073), true, true),
+	MOOR(7, new Color(0xff0e87cc), false, true), // TODO: color
+	MOORINNER(7, new Color(0xff0e87cc), false, true), // TODO: color
+	MOORBORDER(9, new Color(0xff0e87cc), false, false), // TODO: color
+	FLATTENED_DESERT(217, new Color(0xff949200), false, false),
+	SHARP_FLATTENED_DESERT(217, new Color(0xff949200), false, false),
+	GRAVEL(230, new Color(0xff000000), false, false); // TODO: color
 
 	public static final ELandscapeType[] values = ELandscapeType.values();
 
-	private final int image;
-	private final Color color;
-	private final boolean isWater;
+	public final int image;
+	public final Color color;
+	public final boolean isWater;
+	public final boolean isBlocking;
 	public final byte ordinal;
 
-	ELandscapeType(int image, Color color, boolean isWater) {
+	ELandscapeType(int image, Color color, boolean isWater, boolean isBlocking) {
 		this.image = image;
 		this.color = color;
 		this.isWater = isWater;
+		this.isBlocking = isBlocking;
 		this.ordinal = (byte) super.ordinal();
 	}
 
-	public int getImageNumber() {
+	public final int getImageNumber() {
 		return image;
 	}
 
@@ -59,7 +61,7 @@ public enum ELandscapeType {
 	 * 
 	 * @return The color the landscape has.
 	 */
-	public Color getColor() {
+	public final Color getColor() {
 		return color;
 	}
 
@@ -70,11 +72,11 @@ public enum ELandscapeType {
 	 * 
 	 * @return
 	 */
-	public boolean isWater() {
+	public final boolean isWater() {
 		return isWater;
 	}
 
-	public boolean isGrass() {
+	public final boolean isGrass() {
 		return this == GRASS || this == FLATTENED;
 	}
 }
--- src/jsettlers/common/map/IMapData.java
+++ src/jsettlers/common/map/IMapData.java
@@ -7,15 +7,15 @@ import jsettlers.common.position.ShortPoint2D;
 
 public interface IMapData {
 
-	public abstract int getWidth();
+	int getWidth();
 
-	public abstract int getHeight();
+	int getHeight();
 
-	public abstract ELandscapeType getLandscape(int x, int y);
+	ELandscapeType getLandscape(int x, int y);
 
-	public abstract MapObject getMapObject(int x, int y);
+	MapObject getMapObject(int x, int y);
 
-	public abstract byte getLandscapeHeight(int x, int y);
+	byte getLandscapeHeight(int x, int y);
 
 	/**
 	 * Gets the start point of the given player. Always returns a valid point.
@@ -23,19 +23,31 @@ public interface IMapData {
 	 * @param player
 	 * @return
 	 */
-	public abstract ShortPoint2D getStartPoint(int player);
-	
-	public abstract int getPlayerCount();
-	
-	public abstract EResourceType getResourceType(short x, short y);
+	ShortPoint2D getStartPoint(int player);
+
+	int getPlayerCount();
+
+	EResourceType getResourceType(short x, short y);
 
 	/**
 	 * Gets the amount of resources for a given position. In range 0..127
+	 * 
 	 * @param x
 	 * @param y
 	 * @return
 	 */
-	public abstract byte getResourceAmount(short x, short y);
+	byte getResourceAmount(short x, short y);
 
+	/**
+	 * Gets the id of the blocked partition of the given position.
+	 * 
+	 * 
+	 * @param x
+	 *            X coordinate of the position.
+	 * @param y
+	 *            Y coordinate of the position.
+	 * @return The id of the blocked partition the given position belongs to.
+	 */
+	short getBlockedPartition(short x, short y);
 
 }
