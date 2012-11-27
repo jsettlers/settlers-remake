d26b34c41c26c9224fb01a748a80a4f202a86ac4
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2012-11-27 13:50:38 +0000

    - fixed AreaTraversingAlgorithm

---
+++ src/jsettlers/GraphicsGridAdapter.java
@@ -0,0 +1,84 @@
+package jsettlers;
+
+import jsettlers.common.CommonConstants;
+import jsettlers.common.landscape.ELandscapeType;
+import jsettlers.common.map.IGraphicsBackgroundListener;
+import jsettlers.common.map.IGraphicsGrid;
+import jsettlers.common.mapobject.IMapObject;
+import jsettlers.common.movable.IMovable;
+
+public class GraphicsGridAdapter implements IGraphicsGrid {
+
+	private short width;
+	private short height;
+
+	public GraphicsGridAdapter(int width, int height) {
+		this.width = (short) width;
+		this.height = (short) height;
+	}
+
+	@Override
+	public short getWidth() {
+		return width;
+	}
+
+	@Override
+	public short getHeight() {
+		return height;
+	}
+
+	@Override
+	public IMovable getMovableAt(int x, int y) {
+		return null;
+	}
+
+	@Override
+	public IMapObject getMapObjectsAt(int x, int y) {
+		return null;
+	}
+
+	@Override
+	public byte getHeightAt(int x, int y) {
+		return 0;
+	}
+
+	@Override
+	public ELandscapeType getLandscapeTypeAt(int x, int y) {
+		return ELandscapeType.GRASS;
+	}
+
+	@Override
+	public int getDebugColorAt(int x, int y) {
+		return -1;
+	}
+
+	@Override
+	public boolean isBorder(int x, int y) {
+		return false;
+	}
+
+	@Override
+	public byte getPlayerIdAt(int x, int y) {
+		return 0;
+	}
+
+	@Override
+	public byte getVisibleStatus(int x, int y) {
+		return CommonConstants.FOG_OF_WAR_VISIBLE;
+	}
+
+	@Override
+	public boolean isFogOfWarVisible(int x, int y) {
+		return true;
+	}
+
+	@Override
+	public void setBackgroundListener(IGraphicsBackgroundListener backgroundListener) {
+	}
+
+	@Override
+	public int nextDrawableX(int x, int y, int maxX) {
+		return x + 1;
+	}
+
+}
---
+++ src/jsettlers/algorithms/traversing/area/AreaTraversingAlgorithmTest.java
@@ -0,0 +1,61 @@
+package jsettlers.algorithms.traversing.area;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.BitSet;
+import java.util.LinkedList;
+
+import jsettlers.common.map.shapes.FreeMapArea;
+import jsettlers.common.map.shapes.MapCircle;
+import jsettlers.common.position.ShortPoint2D;
+import jsettlers.logic.algorithms.traversing.ITraversingVisitor;
+import jsettlers.logic.algorithms.traversing.area.AreaTraversingAlgorithm;
+import jsettlers.logic.algorithms.traversing.borders.IContainingProvider;
+
+import org.junit.Test;
+
+public class AreaTraversingAlgorithmTest {
+
+	private static final int WIDTH = 200;
+	private static final int HEIGHT = 200;
+
+	@Test
+	public void testTraversing1() {
+		final MapCircle c1 = new MapCircle(100, 100, 50);
+		final MapCircle c2 = new MapCircle(120, 100, 20);
+		final MapCircle c3 = new MapCircle(120, 100, 10);
+
+		final IContainingProvider containingProvider = new IContainingProvider() {
+			@Override
+			public boolean contains(int x, int y) {
+				return c1.contains(x, y) && !c2.contains(x, y) || c3.contains(x, y);
+			}
+		};
+
+		final LinkedList<ShortPoint2D> area = new LinkedList<ShortPoint2D>();
+		final BitSet visited = new BitSet(WIDTH * HEIGHT);
+		ITraversingVisitor visitor = new ITraversingVisitor() {
+			@Override
+			public boolean visit(int x, int y) {
+				assertTrue(c1.contains(x, y) && !c2.contains(x, y)); // checks if the position is in the area
+				area.add(new ShortPoint2D(x, y));
+				int idx = x + y * WIDTH;
+				assertFalse(visited.get(idx)); // every position is only visited once
+				visited.set(idx);
+				return true;
+			}
+		};
+
+		boolean result = AreaTraversingAlgorithm.traverseArea(containingProvider, visitor, c1.iterator().next(), WIDTH, HEIGHT);
+		assertTrue(result);
+
+		// check if all positions in the area have been traversed
+		FreeMapArea mapArea = new FreeMapArea(area);
+		for (ShortPoint2D curr : c1) {
+			if (!c2.contains(curr)) {
+				assertTrue(mapArea.contains(curr));
+			}
+		}
+	}
+}
