2856c62915ceebaff7c1b79a33eb053e379ef069
Michael Zangl <michael>
2012-12-03 18:00:58 +0000

    Changed TestWindow to use native GL.

--- src/jsettlers/TestWindow.java
+++ src/jsettlers/TestWindow.java
@@ -1,16 +1,14 @@
 package jsettlers;
 
+import go.graphics.nativegl.NativeAreaWindow;
 import go.graphics.sound.ISoundDataRetriever;
 import go.graphics.sound.SoundPlayer;
-import go.graphics.swing.AreaContainer;
-
-import javax.swing.JFrame;
 
 import jsettlers.common.map.IGraphicsGrid;
 import jsettlers.common.resources.ResourceManager;
 import jsettlers.graphics.action.Action;
 import jsettlers.graphics.action.EActionType;
-import jsettlers.graphics.action.SelectAction;
+import jsettlers.graphics.action.PointAction;
 import jsettlers.graphics.map.IMapInterfaceListener;
 import jsettlers.graphics.map.MapInterfaceConnector;
 import jsettlers.graphics.map.draw.ImageProvider;
@@ -30,8 +28,6 @@ public class TestWindow {
 	public static MapInterfaceConnector openTestWindow(IGraphicsGrid map) {
 		ImageProvider.getInstance().startPreloading();
 
-		JFrame window = new JFrame("window test");
-
 		JOGLPanel content = new JOGLPanel(new SoundPlayer() {
 			@Override
 			public void playSound(int sound, float lvolume, float rvolume) {
@@ -39,17 +35,20 @@ public class TestWindow {
 			}
 
 			@Override
-			public void setSoundDataRetriever(ISoundDataRetriever soundDataRetriever) {
+			public void setSoundDataRetriever(
+			        ISoundDataRetriever soundDataRetriever) {
 				// TODO Auto-generated method stub
 			}
 		});
-		window.add(new AreaContainer(content.getArea()));
+		/*
+		 * JFrame window = new JFrame("window test"); window.add(new
+		 * AreaContainer(content.getArea())); window.pack();
+		 * window.setSize(1300, 800); window.setLocationRelativeTo(null);
+		 * window.setVisible(true);
+		 * window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 */
 
-		window.pack();
-		window.setSize(1300, 800);
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		new NativeAreaWindow(content.getArea());
 
 		MapInterfaceConnector connector = content.showGameMap(map, null);
 
@@ -58,7 +57,7 @@ public class TestWindow {
 			@Override
 			public void action(Action action) {
 				if (action.getActionType() == EActionType.SELECT_POINT) {
-					SelectAction selectAction = (SelectAction) action;
+					PointAction selectAction = (PointAction) action;
 
 					System.out.println("Action preformed: " + action.getActionType() + " at: " + selectAction.getPosition());
 				} else {
