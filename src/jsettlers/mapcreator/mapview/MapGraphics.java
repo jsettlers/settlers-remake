c1b1e67528f3796f61cb3b279f2fe91b0d0cdb7a
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2013-04-06 14:32:12 +0000

    - added action and action handling for setting the material distribution - extended IGraphicsGrid interface to give the UI access to the settings of a partition

--- src/jsettlers/mapcreator/mapview/MapGraphics.java
+++ src/jsettlers/mapcreator/mapview/MapGraphics.java
@@ -5,6 +5,7 @@ import jsettlers.common.CommonConstants;
 import jsettlers.common.landscape.ELandscapeType;
 import jsettlers.common.map.IGraphicsBackgroundListener;
 import jsettlers.common.map.IGraphicsGrid;
+import jsettlers.common.map.partition.IPartitionSettings;
 import jsettlers.common.mapobject.IMapObject;
 import jsettlers.common.movable.IMovable;
 import jsettlers.mapcreator.data.MapData;
@@ -100,6 +101,11 @@ public class MapGraphics implements IGraphicsGrid {
 	@Override
 	public int nextDrawableX(int x, int y, int maxX) {
 		return x + 1;
+	}
+
+	@Override
+	public IPartitionSettings getPartitionSettings(int x, int y) {
+		return null;
 	}
 
 }
