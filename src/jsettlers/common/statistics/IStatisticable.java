7bc4f6911919dd44e6ecc368020fe7e011aba5e4
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2013-04-06 10:19:26 +0000

    - added new method getGameTime() to IStatisticable interface to supply UI with the game time (#issue 0112)

--- src/jsettlers/common/statistics/IStatisticable.java
+++ src/jsettlers/common/statistics/IStatisticable.java
@@ -3,7 +3,20 @@ package jsettlers.common.statistics;
 import jsettlers.common.material.EMaterialType;
 import jsettlers.common.movable.EMovableType;
 
+/**
+ * This interface supplies statistical information about the game to the UI.
+ * 
+ * @author Andreas Eberle
+ * 
+ */
 public interface IStatisticable {
+	/**
+	 * Gets the game time.
+	 * 
+	 * @return The current game time in milliseconds.
+	 */
+	int getGameTime();
+
 	int getNumberOf(EMaterialType materialType);
 
 	int getNumberOf(EMovableType movableType);
