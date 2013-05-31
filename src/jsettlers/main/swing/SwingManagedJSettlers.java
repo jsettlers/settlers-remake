33402ab86a27cecabba649880060e825c3c8a781
Andreas Eberle <andreas-eberle@settlers-android-clone.com>
2013-05-31 13:32:13 +0000

    - started to change JSettlers to use the new networklib WARNING: Multiplayer is not working at the moment!

--- src/jsettlers/main/swing/SwingManagedJSettlers.java
+++ src/jsettlers/main/swing/SwingManagedJSettlers.java
@@ -8,23 +8,18 @@ import java.awt.Dimension;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JFrame;
 
 import jsettlers.common.CommonConstants;
-import jsettlers.common.map.IMapDataProvider;
 import jsettlers.common.resources.ResourceManager;
 import jsettlers.graphics.ISettlersGameDisplay;
 import jsettlers.graphics.map.draw.ImageProvider;
 import jsettlers.graphics.swing.JOGLPanel;
 import jsettlers.graphics.swing.SwingResourceLoader;
 import jsettlers.graphics.swing.SwingResourceProvider;
-import jsettlers.main.JSettlersGame;
 import jsettlers.main.ManagedJSettlers;
-import jsettlers.main.MapDataMapCreator;
-import network.NetworkManager;
 
 public class SwingManagedJSettlers {
 
@@ -41,7 +36,7 @@ public class SwingManagedJSettlers {
 	 * @throws ClassNotFoundException
 	 */
 	public static void main(String[] args) throws FileNotFoundException,
-	        IOException, ClassNotFoundException {
+			IOException, ClassNotFoundException {
 		List<String> argsList = Arrays.asList(args);
 
 		loadDebugSettings(argsList);
@@ -51,9 +46,6 @@ public class SwingManagedJSettlers {
 		game.start(getGui(argsList));
 
 		ImageProvider.getInstance().startPreloading();
-
-		// NetworkTimer.loadLogging("logs/2012_12_19-07_44_01.log");
-		// NetworkTimer.activateLogging("logs");
 	}
 
 	private static void loadDebugSettings(List<String> argsList) {
@@ -61,21 +53,6 @@ public class SwingManagedJSettlers {
 			CommonConstants.ENABLE_ALL_PLAYER_FOG_OF_WAR = true;
 			CommonConstants.ENABLE_ALL_PLAYER_SELECTION = true;
 		}
-	}
-
-	/**
-	 * Directly starts a map window
-	 * 
-	 * @param mapname
-	 */
-	public static void startMap(IMapDataProvider data) {
-		ResourceManager.setProvider(new SwingResourceProvider());
-		// TODO: detect exit
-		JSettlersGame game =
-		        new JSettlersGame(getGui(Collections.<String> emptyList()),
-		                new MapDataMapCreator(data), 123456L,
-		                new NetworkManager(), (byte) 0);
-		game.start();
 	}
 
 	/**
