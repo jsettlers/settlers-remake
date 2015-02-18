d946eb691191ad073b42a74974e8348ee3e037db
Andreas Eberle <email@andreas-eberle.com>
2015-02-18 13:26:20 +0100

    Added building of CommitInfo to jsettlers.common

--- jsettlers.main.swing/src/jsettlers/main/swing/SwingManagedJSettlers.java
+++ jsettlers.main.swing/src/jsettlers/main/swing/SwingManagedJSettlers.java
@@ -14,6 +14,7 @@ import java.util.TimerTask;
 
 import javax.swing.JFrame;
 
+import jsettlers.common.CommitInfo;
 import jsettlers.common.CommonConstants;
 import jsettlers.common.map.MapLoadException;
 import jsettlers.common.resources.ResourceManager;
@@ -37,7 +38,7 @@ import networklib.client.OfflineNetworkConnector;
  */
 public class SwingManagedJSettlers {
 
-	private static final String BUILD = "build - ???";
+	private static final String BUILD = "commit: " + CommitInfo.COMMIT_HASH_SHORT;
 
 	/**
 	 * @param args
