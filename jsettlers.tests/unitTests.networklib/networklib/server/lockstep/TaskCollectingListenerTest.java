f0a162f99790aa030349782c1f50d58a7ea2844b
Andreas Eberle <email@andreas-eberle.com>
2015-02-24 14:10:19 +0100

    Refactored variable naming in TaskCollectingListenerTest.

--- jsettlers.tests/unitTests.networklib/networklib/server/lockstep/TaskCollectingListenerTest.java
+++ jsettlers.tests/unitTests.networklib/networklib/server/lockstep/TaskCollectingListenerTest.java
@@ -26,38 +26,35 @@ import org.junit.Test;
  * 
  */
 public class TaskCollectingListenerTest {
-	private Channel c1;
-	private Channel c2;
+	private Channel client;
+	private Channel server;
 
 	@Before
 	public void setUp() throws IOException {
 		Channel[] channels = TestUtils.setUpLoopbackChannels();
-		c1 = channels[0];
-		c2 = channels[1];
+		client = channels[0];
+		server = channels[1];
 	}
 
 	@After
 	public void tearDown() {
-		c1.close();
-		c2.close();
+		client.close();
+		server.close();
 	}
 
 	@Test
 	public void testSendAndReceive() throws InterruptedException {
-		// c1 is the channel of the client
-		// c2 is the channel of the server
-
 		BufferingPacketListener<TaskPacket> clientListener = new BufferingPacketListener<TaskPacket>(
 				NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, TaskPacket.DEFAULT_DESERIALIZER);
-		c1.registerListener(clientListener);
+		client.registerListener(clientListener);
 
 		TaskCollectingListener serverListener = new TaskCollectingListener();
-		c2.registerListener(serverListener);
+		server.registerListener(serverListener);
 
 		TestTaskPacket testPacket1 = new TestTaskPacket("TestMessage42", 4711, (byte) -3);
-		c1.sendPacket(NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, testPacket1); // send packet 1 to server
+		client.sendPacket(NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, testPacket1); // send packet 1 to server
 		TestTaskPacket testPacket2 = new TestTaskPacket("Bla B�b B�n0928�38(/�/)\"=$(;:I\"H))!", -2342323, (byte) 4);
-		c1.sendPacket(NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, testPacket2); // send packet 2 to server
+		client.sendPacket(NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, testPacket2); // send packet 2 to server
 
 		Thread.sleep(10);
 
@@ -68,7 +65,7 @@ public class TaskCollectingListenerTest {
 		assertEquals(0, serverListener.getAndResetTasks().size());
 
 		for (ServersideTaskPacket curr : serversideTaskPackets) { // send packets back to client
-			c2.sendPacket(NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, curr);
+			server.sendPacket(NetworkConstants.ENetworkKey.SYNCHRONOUS_TASK, curr);
 		}
 
 		Thread.sleep(10);
