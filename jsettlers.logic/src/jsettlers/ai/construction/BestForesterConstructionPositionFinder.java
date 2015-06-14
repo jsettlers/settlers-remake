da0cb9487a06aea9a62c7a5ca8d7288446ccd982
Stephan Bauer <berlin.stephan@gmail.com>
2015-06-14 15:23:17 +0200

    add documentation and licence

--- jsettlers.logic/src/jsettlers/ai/construction/BestForesterConstructionPositionFinder.java
+++ jsettlers.logic/src/jsettlers/ai/construction/BestForesterConstructionPositionFinder.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2015
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
 package jsettlers.ai.construction;
 
 import java.util.ArrayList;
@@ -8,6 +22,17 @@ import jsettlers.algorithms.construction.AbstractConstructionMarkableMap;
 import jsettlers.common.buildings.EBuildingType;
 import jsettlers.common.position.ShortPoint2D;
 
+/**
+ * Assumptions: foresters are placed after lumberjacks were placed
+ * 
+ * Algorithm: find all possible construction points within the borders of the player - calculates a score (0% bad position till 100% bad position) and
+ * take the position with the lowest bad score - score is affected by distance to the most near lumberjack (0% bad position means the most near lumber
+ * jack is next to this position, 100% bad position means the most near lumber jack is in comparison to the other positions most far away) - score is
+ * affected by distance to other foresters in order to build new foresters to needily lumberjacks (0% bad position means that the most nearest
+ * forester's distance is greater than 25, 100% bad position means that the most nearest forester is just next to the construction position)
+ * 
+ * @author codingberlin
+ */
 public class BestForesterConstructionPositionFinder implements IBestConstructionPositionFinder {
 
 	private static final Double MIN_FORESTER_DISTANCE = new Double(25);
--- jsettlers.logic/src/jsettlers/ai/highlevel/IWhatToDoAi.java
+++ jsettlers.logic/src/jsettlers/ai/highlevel/IWhatToDoAi.java
@@ -1,7 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2015
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
 package jsettlers.ai.highlevel;
 
+/**
+ * The purpose of the higher level IWhatToDoAi is to decide WHAT is to do. It delegates the decision WHERE it is to do to the lower level KI. For
+ * example if the AI player has no stone cutters the WhatToDoAi decides to build with high priority a stone cutter because otherwise the AI player
+ * would be unable to build more houses without stones. Then the WhatToDoAi gets a BestConstructionPositionFinder for Stonecutters and asks it to find
+ * the best place to put a stone cutter on the map - which means near some stones ;-) - and builds the stone cutter there.
+ * 
+ * The separation of high level and low level AI enables the programmer to think in more abstract levels instead of going into details while
+ * programming strategic AIs
+ * 
+ * @author codingberlin
+ */
 public interface IWhatToDoAi {
-	
+
 	abstract void applyRules();
-	
+
 }
