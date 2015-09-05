e5623d11ecf29cf1db8dd0db5cc250b9dc759ba4
Andreas Eberle <email@andreas-eberle.com>
2015-09-05 23:02:21 +0200

    Reworked automated tool production and worker creation.

--- jsettlers.logic/src/jsettlers/logic/map/grid/partition/manager/objects/IWorkerCreationRequest.java
+++ jsettlers.logic/src/jsettlers/logic/map/grid/partition/manager/objects/IWorkerCreationRequest.java
@@ -18,26 +18,10 @@ import java.io.Serializable;
 
 import jsettlers.common.movable.EMovableType;
 import jsettlers.common.position.ILocatable;
-import jsettlers.common.position.ShortPoint2D;
 
-public final class WorkerCreationRequest implements ILocatable, Serializable {
-	private static final long serialVersionUID = 3047014371520017602L;
+public interface IWorkerCreationRequest extends ILocatable, Serializable {
 
-	public final EMovableType movableType;
-	public final ShortPoint2D position;
+	boolean isRequestAlive();
 
-	public WorkerCreationRequest(EMovableType movableType, ShortPoint2D position) {
-		this.movableType = movableType;
-		this.position = position;
-	}
-
-	@Override
-	public String toString() {
-		return movableType + "    " + position;
-	}
-
-	@Override
-	public ShortPoint2D getPos() {
-		return position;
-	}
+	EMovableType requestedMovableType();
 }
