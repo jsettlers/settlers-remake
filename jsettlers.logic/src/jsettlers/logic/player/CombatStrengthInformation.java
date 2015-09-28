cc49d93b59b44107060979b58d0bac74728d343b
Stephan Bauer <berlin.stephan@gmail.com>
2015-09-28 07:01:38 +0200

    store the value instead the index

--- jsettlers.logic/src/jsettlers/logic/player/CombatStrengthInformation.java
+++ jsettlers.logic/src/jsettlers/logic/player/CombatStrengthInformation.java
@@ -21,13 +21,13 @@ public class CombatStrengthInformation implements ICombatStrengthInformation, Se
 			842, 858, 874, 890, 907, 924, 941, 958, 975, 993, 1011, 1029, 1047, 1065, 1084, 1103, 1122, 1141};
 	private final byte playerId;
 	private final PartitionsGrid partitionsGrid;
-	private final int startAmountIndex;
+	private final float startAmount;
 	private static final float[] START_AMOUNT = {652, 600, 504, 444, 396, 348, 312, 300, 276, 252, 228, 216, 204, 192, 180, 168, 163, 156, 149, 144};
 
 	public CombatStrengthInformation(byte playerId, PartitionsGrid partitionsGrid, byte sumOfPlayers) {
 		this.playerId = playerId;
 		this.partitionsGrid = partitionsGrid;
-		this.startAmountIndex = sumOfPlayers - 1;
+		this.startAmount = START_AMOUNT[sumOfPlayers - 1];
 	}
 
 	@Override public float getCombatStrengthAtPosition(ShortPoint2D position) {
@@ -42,7 +42,7 @@ public class CombatStrengthInformation implements ICombatStrengthInformation, Se
 	}
 
 	@Override public float getCombatStrength() {
-		float amountOfGold = START_AMOUNT[startAmountIndex];
+		float amountOfGold = startAmount;
 		for (Partition playersPartition : partitionsGrid.getPartitionsOfPlayerId(playerId)) {
 			amountOfGold += playersPartition.getMaterialCounts().getAmountOf(EMaterialType.GOLD);
 		}
