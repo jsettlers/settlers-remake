ad35c500934e765a30ac516a1c53dc9d89fcf9a6
Andreas Eberle <email@andreas-eberle.com>
2016-01-16 15:10:34 +0100

    Extracted manual test of ExceptionDialog to a tester class in jsettlers.tests/tools/.

--- jsettlers.mapcreator/src/jsettlers/exceptionhandler/ExceptionDialog.java
+++ jsettlers.mapcreator/src/jsettlers/exceptionhandler/ExceptionDialog.java
@@ -22,8 +22,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
 
 import jsettlers.exceptionhandler.localization.ExceptionLabels;
 
@@ -175,36 +173,4 @@ public class ExceptionDialog extends JFrame {
 		p.add(new JScrollPane(txt), BorderLayout.CENTER);
 		return p;
 	}
-
-	/**
-	 * To generate an exception with a big stacktrace...
-	 */
-	private static void endlessRecursion() {
-		endlessRecursion();
-	}
-
-	/**
-	 * Main to test the dialog
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
-		} catch (Exception e) {
-			// could not be loaded, ignore error
-		}
-
-		try {
-			endlessRecursion();
-		} catch (Throwable e) {
-			ExceptionHandler.displayError(e, "test erro");
-		}
-	}
-
 }
