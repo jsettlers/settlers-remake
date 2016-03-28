f6cce50dfd3c565dc711401a8b5ae0ef4754525f
Andreas Eberle <email@andreas-eberle.com>
2016-03-28 22:11:19 +0200

    Fixed saving by making MultiRequestStackSharedData serializable.

--- jsettlers.logic/src/jsettlers/logic/buildings/stack/multi/MultiRequestStack.java
+++ jsettlers.logic/src/jsettlers/logic/buildings/stack/multi/MultiRequestStack.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2015
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
 package jsettlers.logic.buildings.stack.multi;
 
 import jsettlers.common.buildings.EBuildingType;
@@ -10,6 +24,15 @@ import jsettlers.logic.buildings.stack.IRequestsStackGrid;
 import jsettlers.logic.constants.Constants;
 import jsettlers.logic.map.grid.partition.manager.materials.requests.MaterialRequestObject;
 
+/**
+ * A stack that is capable of requesting multiple materials at the same time. When one of the requested materials will be delivered, the stack only
+ * requests that materials as long as it holds any of that material.
+ * <p />
+ * Therefore the stack can only hold a single type of material.
+ * 
+ * @author Andreas Eberle
+ *
+ */
 public class MultiRequestStack implements IRequestStack {
 	private static final long serialVersionUID = 1735769845576581676L;
 
