957f22659fa6b879ce1920f4b79a8c3d9abf9547
Stephan Bauer <berlin.stephan@gmail.com>
2016-05-08 13:46:55 +0200

    add documentation

--- jsettlers.logic/src/main/java/jsettlers/ai/highlevel/AiMapInformation.java
+++ jsettlers.logic/src/main/java/jsettlers/ai/highlevel/AiMapInformation.java
@@ -25,31 +25,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class calculates information about the map for the AI
- * 
+ * This class calculates information about the map for the AI. At the moment it calculates how many buildings of a building type can be build on
+ * the map by each player. In a nutshell it divides the landscape and resources of the map by the number of possible players. Then it determines
+ * the maximal possible iron mines and coal mines. Depending of mines to smiths ratio it calculates the maximal number of possible smiths. Then it
+ * calculates the needed amount of food building by preferring fisher huts over farms. At least it calculates how many lumberjacks and stone
+ * cutters are needed to build this economy including the necessary living houses to levy soldiers. Now it checks if the calculated buildings have
+ * enough space to be build on the map. If not it reduces the number of smiths unless it hits a threshold. Then it reduces the number of gold smith,
+ * winegrowers and big temples to a minimum set of this buildings. If again this are too much buildings. It keeps reducing smiths.
+ *
+ * TODO: add information where to find the resources and how far they are to let AI use pioneers instead of towers
+ *
  * @author codingberlin
  */
 public class AiMapInformation {
 
-	private static final short FISHERMAN_DIVISOR = 40;
-	private static final short COAL_MINE_DIVISOR = 120;
-	private static final short IRON_MINE_DIVISOR = 50;
-	private static final int COAL_TO_IRON_FACTOR = 2;
-	private static final double WEAPON_SMITH_BARRACKS_RATIO = 3;
-	private static final double WEAPON_SMITH_FISHER_RATIO = 3F / 2F;
-	private static final double WEAPON_SMITH_FARM_RATIO = 1F / 2F;
-	private static final double FARM_BAKER_RATIO = 1F / 3F;
-	private static final double FARM_MILL_RATIO = 1F / 6F;
-	private static final double FARM_WATERWORKS_RATIO = 1F / 3F;
-	private static final double FARM_PIG_FARM_RATIO = 1F / 3F;
-	private static final double FARM_SLAUGHTER_RATIO = 1F / 6F;
+	private static final double FISH_TO_FISHER_HUTS_RATIO = 40F / 1F;
+	private static final double COAL_TO_COAL_MINES_RATIO = 120F / 1F;
+	private static final double IRONORE_TO_IRON_MINES_RATIO = 50F / 1F;
+	private static final double COAL_MINE_TO_IRONORE_MINE_RATIO = 2F / 1F;
+	private static final double WEAPON_SMITH_TO_BARRACKS_RATIO = 3F / 1F;
+	private static final double WEAPON_SMITH_TO_FISHER_HUT_RATIO = 3F / 2F;
+	private static final double WEAPON_SMITH_TO_FARM_RATIO = 1F / 2F;
+	private static final double FARM_TO_BAKER_RATIO = 1F / 3F;
+	private static final double FARM_TO_MILL_RATIO = 1F / 6F;
+	private static final double FARM_TO_WATERWORKS_RATIO = 1F / 3F;
+	private static final double FARM_TO_PIG_FARM_RATIO = 1F / 3F;
+	private static final double FARM_TO_SLAUGHTER_RATIO = 1F / 6F;
+	private static final double WEAPON_SMITH_TO_LUMBERJACK_RATIO = 6F / 8F;
+	private static final double LUMBERJACK_TO_SAWMILL_RATIO = 1F / 2F;
+	private static final double LUMBERJACK_TO_FORESTER_RATIO = 1F / 2F;
+	private static final double LUMBERJACK_TO_STONE_CUTTER_RATIO = 5F / 8F;
+	public static final float COAL_MINE_TO_SMITH_RATIO = 9F / 5F;
 	private static final int MIN_SMITHS_BEFORE_WINE_AND_GOLD_REDUCTION = 10;
 	private static final int MIN_WINE_GROWER_BEFORE_GOLD_REDUCTION = 2;
-	private static final double WEAPON_SMITH_LUMBERJACK_RATIO = 6F / 8F;
-	private static final double LUMBERJACK_SAWMILL_RATIO = 1F / 2F;
-	private static final double LUMBERJACK_FORESTER_RATIO = 1F / 2F;
-	private static final double LUMBERJACK_STONE_CUTTER_RATIO = 5F / 8F;
-	public static final float COAL_MINE_TO_SMITH_RATIO = 1.8F;
+	// max 10 fisher to prevent AI from building only fishermen which on the one hand looks very unnatural and on the other hand is unproductive in
+	// the late game caused by over fishing.
+	public static final int MAX_FISHERS = 10;
 	private List<BuildingCount> buildingCounts;
 	private int numberOfFisher;
 	private int numberOfWeaponSmiths;
@@ -83,6 +94,10 @@ public class AiMapInformation {
 		return numberOfBigTemples;
 	}
 
+	public int getNumberOfLumberJacks() {
+		return numberOfLumberJacks;
+	}
+
 	public AiMapInformation(MainGrid mainGrid) {
 		LandscapeGrid landscapeGrid = mainGrid.getLandscapeGrid();
 
@@ -104,16 +119,15 @@ public class AiMapInformation {
 		}
 		int numberOfPlayers = mainGrid.getPartitionsGrid().getNumberOfPlayers();
 		grasTiles = grasTiles / numberOfPlayers;
-		// maximal 10 Fisher to prevent AI from building only Fishermen which looks very unnatural
-		int maxFishermen = Math.min(10, (int) (resourcesCount[EResourceType.FISH.ordinal] / numberOfPlayers) / FISHERMAN_DIVISOR);
-		int maxCoalMines = (int) (resourcesCount[EResourceType.COAL.ordinal] / numberOfPlayers) / COAL_MINE_DIVISOR;
-		int maxIronMines = (int) (resourcesCount[EResourceType.IRONORE.ordinal] / numberOfPlayers) / IRON_MINE_DIVISOR;
+		int maxFishermen = Math.min(MAX_FISHERS, (int) Math.round((resourcesCount[EResourceType.FISH.ordinal] / numberOfPlayers) / FISH_TO_FISHER_HUTS_RATIO));
+		int maxCoalMines = (int) Math.round((resourcesCount[EResourceType.COAL.ordinal] / numberOfPlayers) / COAL_TO_COAL_MINES_RATIO);
+		int maxIronMines = (int) Math.round((resourcesCount[EResourceType.IRONORE.ordinal] / numberOfPlayers) / IRONORE_TO_IRON_MINES_RATIO);
 		int maxGoldMelts = resourcesCount[EResourceType.GOLDORE.ordinal] > 0 ? 2 : 0;
 
-		if (maxIronMines > maxCoalMines / COAL_TO_IRON_FACTOR + 1)
-			maxIronMines = maxCoalMines / COAL_TO_IRON_FACTOR + 1;
-		if (maxCoalMines > maxIronMines * COAL_TO_IRON_FACTOR + 1)
-			maxCoalMines = maxIronMines * COAL_TO_IRON_FACTOR + 1;
+		if (maxIronMines > maxCoalMines / COAL_MINE_TO_IRONORE_MINE_RATIO + 1)
+			maxIronMines = (int) Math.round(maxCoalMines / COAL_MINE_TO_IRONORE_MINE_RATIO + 1);
+		if (maxCoalMines > maxIronMines * COAL_MINE_TO_IRONORE_MINE_RATIO + 1)
+			maxCoalMines = (int) Math.round(maxIronMines * COAL_MINE_TO_IRONORE_MINE_RATIO + 1);
 		int maxSmiths = (int) Math.floor((float) maxCoalMines * COAL_MINE_TO_SMITH_RATIO);
 		calculateBuildingCounts(maxSmiths, maxFishermen, maxGoldMelts, 3, 1, grasTiles);
 	}
@@ -126,29 +140,29 @@ public class AiMapInformation {
 			int numberOfWeaponSmiths, int maxFishermen, int maxGoldMelts, int maxWineGrower, int maxBigTemples, long grasTiles) {
 		List<BuildingCount> buildingCounts = new ArrayList<BuildingCount>();
 		buildingCounts.add(new BuildingCount(EBuildingType.COALMINE, numberOfWeaponSmiths));
-		buildingCounts.add(new BuildingCount(EBuildingType.IRONMINE, numberOfWeaponSmiths / COAL_TO_IRON_FACTOR + 1));
+		buildingCounts.add(new BuildingCount(EBuildingType.IRONMINE, (int) Math.round(numberOfWeaponSmiths / COAL_MINE_TO_IRONORE_MINE_RATIO + 1)));
 		buildingCounts.add(new BuildingCount(EBuildingType.IRONMELT, numberOfWeaponSmiths));
 		buildingCounts.add(new BuildingCount(EBuildingType.WEAPONSMITH, numberOfWeaponSmiths));
-		buildingCounts.add(new BuildingCount(EBuildingType.BARRACK, (int) Math.ceil((double) numberOfWeaponSmiths / WEAPON_SMITH_BARRACKS_RATIO)));
+		buildingCounts.add(new BuildingCount(EBuildingType.BARRACK, (int) Math.ceil((double) numberOfWeaponSmiths / WEAPON_SMITH_TO_BARRACKS_RATIO)));
 		buildingCounts.add(new BuildingCount(EBuildingType.TOOLSMITH, 1));
 
-		int numberOfFisher = Math.min((int) (numberOfWeaponSmiths * WEAPON_SMITH_FISHER_RATIO), maxFishermen);
+		int numberOfFisher = Math.min((int) (numberOfWeaponSmiths * WEAPON_SMITH_TO_FISHER_HUT_RATIO), maxFishermen);
 		buildingCounts.add(new BuildingCount(EBuildingType.FISHER, numberOfFisher));
-		int numberOfRemainingWeaponSmithsRest = Math.max(0, numberOfWeaponSmiths - (int) (numberOfFisher / WEAPON_SMITH_FISHER_RATIO));
+		int numberOfRemainingWeaponSmithsRest = Math.max(0, numberOfWeaponSmiths - (int) (numberOfFisher / WEAPON_SMITH_TO_FISHER_HUT_RATIO));
 
-		int numberOfFarms = (int) (numberOfRemainingWeaponSmithsRest * WEAPON_SMITH_FARM_RATIO);
+		int numberOfFarms = (int) (numberOfRemainingWeaponSmithsRest * WEAPON_SMITH_TO_FARM_RATIO);
 		buildingCounts.add(new BuildingCount(EBuildingType.FARM, numberOfFarms));
-		buildingCounts.add(new BuildingCount(EBuildingType.BAKER, (int) (numberOfFarms * FARM_BAKER_RATIO)));
-		buildingCounts.add(new BuildingCount(EBuildingType.MILL, (int) (numberOfFarms * FARM_MILL_RATIO)));
-		buildingCounts.add(new BuildingCount(EBuildingType.WATERWORKS, (int) (numberOfFarms * FARM_WATERWORKS_RATIO)));
-		buildingCounts.add(new BuildingCount(EBuildingType.SLAUGHTERHOUSE, (int) (numberOfFarms * FARM_SLAUGHTER_RATIO)));
-		buildingCounts.add(new BuildingCount(EBuildingType.PIG_FARM, (int) (numberOfFarms * FARM_PIG_FARM_RATIO)));
+		buildingCounts.add(new BuildingCount(EBuildingType.BAKER, (int) (numberOfFarms * FARM_TO_BAKER_RATIO)));
+		buildingCounts.add(new BuildingCount(EBuildingType.MILL, (int) (numberOfFarms * FARM_TO_MILL_RATIO)));
+		buildingCounts.add(new BuildingCount(EBuildingType.WATERWORKS, (int) (numberOfFarms * FARM_TO_WATERWORKS_RATIO)));
+		buildingCounts.add(new BuildingCount(EBuildingType.SLAUGHTERHOUSE, (int) (numberOfFarms * FARM_TO_SLAUGHTER_RATIO)));
+		buildingCounts.add(new BuildingCount(EBuildingType.PIG_FARM, (int) (numberOfFarms * FARM_TO_PIG_FARM_RATIO)));
 
-		int numberOfLumberJacks = Math.max((int) (numberOfWeaponSmiths * WEAPON_SMITH_LUMBERJACK_RATIO), 3);
+		int numberOfLumberJacks = Math.max((int) (numberOfWeaponSmiths * WEAPON_SMITH_TO_LUMBERJACK_RATIO), 3);
 		buildingCounts.add(new BuildingCount(EBuildingType.LUMBERJACK, numberOfLumberJacks));
-		buildingCounts.add(new BuildingCount(EBuildingType.FORESTER, Math.max((int) (numberOfLumberJacks * LUMBERJACK_FORESTER_RATIO), 1)));
-		buildingCounts.add(new BuildingCount(EBuildingType.SAWMILL, Math.max((int) (numberOfLumberJacks * LUMBERJACK_SAWMILL_RATIO), 1)));
-		buildingCounts.add(new BuildingCount(EBuildingType.STONECUTTER, Math.max((int) (numberOfLumberJacks * LUMBERJACK_STONE_CUTTER_RATIO), 1)));
+		buildingCounts.add(new BuildingCount(EBuildingType.FORESTER, Math.max((int) (numberOfLumberJacks * LUMBERJACK_TO_FORESTER_RATIO), 1)));
+		buildingCounts.add(new BuildingCount(EBuildingType.SAWMILL, Math.max((int) (numberOfLumberJacks * LUMBERJACK_TO_SAWMILL_RATIO), 1)));
+		buildingCounts.add(new BuildingCount(EBuildingType.STONECUTTER, Math.max((int) (numberOfLumberJacks * LUMBERJACK_TO_STONE_CUTTER_RATIO), 1)));
 
 		if (maxGoldMelts > 0) {
 			buildingCounts.add(new BuildingCount(EBuildingType.GOLDMELT, maxGoldMelts));
@@ -198,9 +212,5 @@ public class AiMapInformation {
 		}
 
 		return true;
-	}
-
-	public int getNumberOfLumberJacks() {
-		return numberOfLumberJacks;
 	}
 }
