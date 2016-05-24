2d2049e6011c18d8c7638821c0da9ac8fab463f3
Andreas Eberle <email@andreas-eberle.com>
2016-05-24 22:55:35 +0200

    Refactored BorderTraversingAlgorithm by extracting the search for an outside position.
    # Conflicts:
    #	jsettlers.logic/src/main/java/jsettlers/algorithms/traversing/borders/BorderTraversingAlgorithm.java

--- jsettlers.logic/src/main/java/jsettlers/algorithms/traversing/borders/BorderTraversingAlgorithm.java
+++ jsettlers.logic/src/main/java/jsettlers/algorithms/traversing/borders/BorderTraversingAlgorithm.java
@@ -33,62 +33,60 @@ public final class BorderTraversingAlgorithm {
 	}
 
 	/**
-	 * Traverses the border of an area defined by the given {@link IContainingProvider} starting at {@link startPos}. The given visitor is called for
-	 * every position on the outside of the area.<br>
-	 * If the {@link startPos} is not surrounded by any position that is not in the area (meaning startPos is not on the border), the traversing can't
-	 * be started and the visitor is never called.
-	 * 
+	 * Traverses the border of an area defined by the given
+	 * {@link IContainingProvider} starting at {@link startPos}. The given
+	 * visitor is called for every position on the outside of the area.<br>
+	 * If the {@link startPos} is not surrounded by any position that is not in
+	 * the area (meaning startPos is not on the border), the traversing can't be
+	 * started and the visitor is never called.
+	 *
 	 * @param containingProvider
-	 *            {@link IContainingProvider} defining the position that are in and the ones that are outside the area.
+	 *            {@link IContainingProvider} defining the position that are in
+	 *            and the ones that are outside the area.
 	 * @param startPos
-	 *            The start position for the traversing. This position must be in the area but at the border!
+	 *            The start position for the traversing. This position must be
+	 *            in the area but at the border!
 	 * @param visitor
-	 *            The visitor that will be called for every border position (a border position is a position outside the border!).
+	 *            The visitor that will be called for every border position (a
+	 *            border position is a position outside the border!).
 	 * @param visitOutside
 	 *            If true the positions on the outside will be visited.<br>
 	 *            If false the inside positions will be visited.
 	 * @param traversedPositions
-	 *            This object will contain the number of traversed positions after the call.
+	 *            This object will contain the number of traversed positions
+	 *            after the call.
 	 * @return true if the whole border has been traversed.<br>
-	 *         false if the traversing has been canceled by the {@link ITraversingVisitor}'s visit() method.
+	 *         false if the traversing has been canceled by the
+	 *         {@link ITraversingVisitor}'s visit() method.
 	 */
-	public static boolean traverseBorder(final IContainingProvider containingProvider, final ShortPoint2D startPos, final ITraversingVisitor visitor,
-			boolean visitOutside, MutableInt traversedPositions) {
+	public static boolean traverseBorder(final IContainingProvider containingProvider, final ShortPoint2D startPos,
+			final ITraversingVisitor visitor, boolean visitOutside, MutableInt traversedPositions) {
+
+		ShortPoint2D outsidePosition = findOutsidePosition(startPos, containingProvider);
+
+		if (outsidePosition == null) {
+			return false; // no outside position found
+		}
+
 		final int startInsideX = startPos.x;
 		final int startInsideY = startPos.y;
+
+		final int startOutsideX = outsidePosition.x;
+		final int startOutsideY = outsidePosition.y;
+
+		if ((visitOutside && !visitor.visit(startOutsideX, startOutsideY))
+				|| (!visitOutside && !visitor.visit(startInsideX, startInsideY))) {
+			traversedPositions.value = 1;
+			return false;
+		}
 
 		int insideX = startInsideX;
 		int insideY = startInsideY;
 
-		int outsideX = -1;
-		int outsideY = -1;
-
-		boolean foundOutsidePos = false;
-
-		// determine first outside position
-		for (EDirection dir : EDirection.VALUES) {
-			outsideX = insideX + dir.gridDeltaX;
-			outsideY = insideY + dir.gridDeltaY;
-
-			if (!containingProvider.contains(outsideX, outsideY)) {
-				foundOutsidePos = true;
-				break;
-			}
-		}
-
-		if (!foundOutsidePos) { // no neighbor of the start position is on the outside.
-			return false;
-		}
-
-		final int startOutsideX = outsideX;
-		final int startOutsideY = outsideY;
+		int outsideX = startOutsideX;
+		int outsideY = startOutsideY;
 
 		int traversedPositionsCounter = 1;
-
-		if (!visitor.visit(startOutsideX, startOutsideY)) {
-			traversedPositions.value = traversedPositionsCounter;
-			return false;
-		}
 
 		do {
 			traversedPositionsCounter++;
@@ -122,8 +120,25 @@ public final class BorderTraversingAlgorithm {
 		return true;
 	}
 
-	public static boolean traverseBorder(final IContainingProvider containingProvider, final ShortPoint2D startPos, final ITraversingVisitor visitor,
-			boolean visitOutside) {
+	private static ShortPoint2D findOutsidePosition(ShortPoint2D insideStartPosition, IContainingProvider containingProvider) {
+		int outsideX = -1;
+		int outsideY = -1;
+
+		// determine first outside position
+		for (EDirection dir : EDirection.VALUES) {
+			outsideX = (int) insideStartPosition.x + dir.gridDeltaX;
+			outsideY = (int) insideStartPosition.y + dir.gridDeltaY;
+
+			if (!containingProvider.contains(outsideX, outsideY)) {
+				return new ShortPoint2D(outsideX, outsideY);
+			}
+		}
+
+		return null; // no neighbor of the start position is on the outside.
+	}
+
+	public static boolean traverseBorder(final IContainingProvider containingProvider, final ShortPoint2D startPos,
+			final ITraversingVisitor visitor, boolean visitOutside) {
 		return traverseBorder(containingProvider, startPos, visitor, visitOutside, new MutableInt());
 	}
 }
