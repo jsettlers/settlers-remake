109b6c91dcda00ba425f45597aeab5ab9ccee6d9
Tom Pratt <tinglybill@outlook.com>
2016-05-27 11:54:45 +0200

    Updated SDK version to marshmallow

---
+++ jsettlers.main.android/src/main/java/jsettlers/main/android/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package jsettlers.main.android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import jsettlers.main.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
