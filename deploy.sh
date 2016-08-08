2f84f4e438f185956fb55f1ca5a92145015044e0
Andreas Eberle <email@andreas-eberle.com>
2016-08-08 19:14:44 +0200

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
@@ -17,51 +18,69 @@ function doCompile {
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=https://github.com/andreas-eberle/settlers-remake.git
+SSH_REPO=git@github.com:andreas-eberle/settlers-remake.git
 SHA=`git rev-parse --verify HEAD`
+
+
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+openssl aes-256-cbc -K $encrypted_af9c5a2dd85c_key -iv $encrypted_af9c5a2dd85c_iv -in deploy_key.enc -out deploy_key -d 
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+
+
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO release
-cd release
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
-echo "1"
+rm -rf gh-pages
+
+git clone $SSH_REPO --branch $TARGET_BRANCH --single-branch gh-pages
+
+
 
 # Clean out existing contents
-rm -rf release/**/* || exit 0
+rm -rf gh-pages/**/* || exit 0
 
 # Run our compile script
 doCompile
 
-# Now let's go have some fun with the cloned repo
-cd release
-git config user.name "Travis CI"
-git config user.email "email@andreas-eberle.com"
 
-echo "2"
+# Create the folder name
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then 
+	FOLDER="pullRequests/$TRAVIS_PULL_REQUEST"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
+elif [ -n "$TRAVIS_TAG" ]; then
+	FOLDER="tags/$TRAVIS_TAG"
+
+else
+	FOLDER="branch/$TRAVIS_BRANCH"
+	
 fi
+
+DATE=`date +%Y-%m-%d_%H-%M-%S`
+FOLDER="gh-pages/$FOLDER/"
+mkdir -v -p "$FOLDER"
+
+
+# Copy the release files into the folder
+cp -R release/ "$FOLDER/${DATE}_$SHA/"
+
+cd gh-pages
+
+
+# Set git config
+git config user.name "Travis CI"
+git config user.email "travis@settlers-remake"
+
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-echo "3"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-openssl aes-256-cbc -K $encrypted_af9c5a2dd85c_key -iv $encrypted_af9c5a2dd85c_iv -in ../deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-echo "4"
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
