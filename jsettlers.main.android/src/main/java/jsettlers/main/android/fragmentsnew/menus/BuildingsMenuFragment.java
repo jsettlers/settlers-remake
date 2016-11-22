d9f383d95d86d22400846df73da5978a90e73605
Tom Pratt <tom.pratt@live.com>
2016-11-22 22:15:37 +0100

    simple recyclerview for buildings in buildingsmenufragment

--- jsettlers.main.android/src/main/java/jsettlers/main/android/fragmentsnew/menus/BuildingsMenuFragment.java
+++ jsettlers.main.android/src/main/java/jsettlers/main/android/fragmentsnew/menus/BuildingsMenuFragment.java
@@ -3,10 +3,12 @@ package jsettlers.main.android.fragmentsnew.menus;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,18 +26,26 @@ public class BuildingsMenuFragment extends Fragment {
         return new BuildingsMenuFragment();
     }
 
-    private List<EBuildingType> buildingTypes;
+    private BuildingsAdapter adapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        buildingTypes = Arrays.asList(BuildingBuildContent.normalBuildings);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu_buildings, container, false);
+        View view = inflater.inflate(R.layout.fragment_menu_buildings, container, false);
+
+        List<EBuildingType> buildingTypes = Arrays.asList(BuildingBuildContent.normalBuildings);
+        adapter = new BuildingsAdapter(buildingTypes);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
 
@@ -43,27 +53,45 @@ public class BuildingsMenuFragment extends Fragment {
      * Adapter
      */
     private class BuildingsAdapter extends RecyclerView.Adapter<BuildingViewHolder> {
+        private List<EBuildingType> buildingTypes;
 
-        @Override
-        public BuildingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
-        }
+        private LayoutInflater layoutInflater;
 
-        @Override
-        public void onBindViewHolder(BuildingViewHolder holder, int position) {
+        public BuildingsAdapter(List<EBuildingType> buildingTypes) {
+            this.buildingTypes = buildingTypes;
 
+            layoutInflater = getActivity().getLayoutInflater();
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return buildingTypes.size();
+        }
+
+        @Override
+        public BuildingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = layoutInflater.inflate(R.layout.item_building, parent, false);
+            BuildingViewHolder buildingViewHolder = new BuildingViewHolder(itemView);
+            return buildingViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(BuildingViewHolder holder, int position) {
+            EBuildingType buildingType = buildingTypes.get(position);
+            holder.setBuildingName(buildingType.name());
         }
     }
 
     private class BuildingViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
 
         public BuildingViewHolder(View itemView) {
             super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.text_view);
+        }
+
+        public void setBuildingName(String name) {
+            textView.setText(name);
         }
     }
 }
