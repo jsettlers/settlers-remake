df74816785d2e51d9b89fcc2d13168e93c588d35
Andreas Eberle <email@andreas-eberle.com>
2016-12-30 00:12:31 +0100

    Add AndroidAutoReplayIT to ensure now unsupported APIs are used in the common code.

--- jsettlers.main.android/src/androidTest/java/jsettlers/main/android/AndroidAutoReplayIT.java
+++ jsettlers.main.android/src/androidTest/java/jsettlers/main/android/AndroidAutoReplayIT.java
@@ -1,22 +1,34 @@
 package jsettlers.main.android;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
-
 import jsettlers.common.CommonConstants;
 import jsettlers.common.map.MapLoadException;
+import jsettlers.common.resources.ResourceManager;
 import jsettlers.integration.replay.AutoReplaySetting;
 import jsettlers.logic.constants.Constants;
 import jsettlers.logic.map.loading.MapLoader;
+import jsettlers.logic.map.loading.list.MapList;
+import jsettlers.main.android.resources.AndroidMapListFactory;
+import jsettlers.main.android.resources.AndroidResourceProvider;
 import jsettlers.main.replay.ReplayUtils;
-import jsettlers.testutils.TestUtils;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.test.InstrumentationTestCase;
 
 /**
  * Created by Andreas Eberle on 29.12.2016.
  */
-public class AndroidAutoReplayIT {
+public class AndroidAutoReplayIT extends InstrumentationTestCase {
+
+	static {
+		System.setProperty("org.xml.sax.driver", "org.xmlpull.v1.sax2.Driver");
+	}
 
 	@BeforeClass
 	public static void setupConstants() {
@@ -24,13 +36,22 @@ public class AndroidAutoReplayIT {
 		CommonConstants.CONTROL_ALL = true;
 		CommonConstants.USE_SAVEGAME_COMPRESSION = true;
 		Constants.FOG_OF_WAR_DEFAULT_ENABLED = false;
+	}
 
-		TestUtils.setupTempResourceManager();
+	public AndroidAutoReplayIT() {
+		Context context = InstrumentationRegistry.getTargetContext();
+
+		File outputDirectory = context.getExternalFilesDir(null);
+		outputDirectory.mkdirs();
+
+		MapList.setDefaultListFactory(new AndroidMapListFactory(context.getAssets(), outputDirectory));
+		AndroidResourceProvider provider = new AndroidResourceProvider(context, outputDirectory);
+		ResourceManager.setProvider(provider);
 	}
 
 	@Test
-	public void testIfReplayWorks() throws MapLoadException, IOException {
+	public void testIfReplayWorks() throws MapLoadException, IOException, ClassNotFoundException {
 		AutoReplaySetting setting = AutoReplaySetting.getDefaultSettings().iterator().next();
-		MapLoader[] savegames = ReplayUtils.replayAndCreateSavegames(setting.getReplayFile(), setting.getTimeMinutes());
+		MapLoader[] saveGames = ReplayUtils.replayAndCreateSavegames(setting.getReplayFile(), new int[] { 10 });
 	}
 }
