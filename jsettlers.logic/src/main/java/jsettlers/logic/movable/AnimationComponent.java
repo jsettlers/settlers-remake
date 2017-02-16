a8547e9ca14083b06edd34741067cae8bb47b958
Jan-Tobias Matysik <jan-tobias.matysik@student.hpi.uni-potsdam.de>
2017-02-16 19:57:54 +0100

    missed file

--- jsettlers.logic/src/main/java/jsettlers/logic/movable/AnimationComponent.java
+++ jsettlers.logic/src/main/java/jsettlers/logic/movable/AnimationComponent.java
@@ -2,38 +2,56 @@ package jsettlers.logic.movable;
 
 import jsettlers.common.movable.EDirection;
 import jsettlers.common.movable.EMovableAction;
+import jsettlers.common.movable.EMovableType;
+import jsettlers.logic.constants.MatchConstants;
 
 /**
  * Created by jt-1 on 2/6/2017.
  */
 
-public class AnimationComponent implements Component {
-    public EMovableAction getAnimation() {
-        //GraphicComponent
-        return null;
+public class AnimationComponent extends Component {
+    private EMovableAction anmiation = EMovableAction.NO_ACTION;
+    private int animationStartTime;
+    private short animationDuration;
+    private boolean isSoundPlayed = false;
+    private boolean isRightStep = false;
+    private EDirection viewDirection;
+
+    public AnimationComponent(EDirection viewDirection) {
+        this.viewDirection = viewDirection;
     }
 
-    public EDirection getDirection() {
-        //GraphicCompontent
-        return null;
+    public EMovableAction getAnimation() {
+        return anmiation;
+    }
+
+    public EDirection getViewDirection() {
+        return viewDirection;
     }
 
     public float getAnimationProgress() {
-        //GraphicComponent
-        return 0;
+        return ((float) (MatchConstants.clock().getTime() - animationStartTime)) / animationDuration;
+    }
+
+    public void startAnimation(EMovableAction animation, float duration) {
+        this.animationStartTime = MatchConstants.clock().getTime();
+        this.animationDuration = (short)(duration*1000);
+        this.anmiation = animation;
     }
 
     public boolean isRightstep() {
-        //GraphicComponent
-        return false;
+        return isRightStep;
+    }
+
+    public void switchStep() {
+        isRightStep = !isRightStep;
     }
 
     public void setSoundPlayed() {
-        //SoundComponent
+        isSoundPlayed = true;
     }
 
     public boolean isSoundPlayed() {
-        //SoundComponent
-        return false;
+        return isSoundPlayed;
     }
 }
