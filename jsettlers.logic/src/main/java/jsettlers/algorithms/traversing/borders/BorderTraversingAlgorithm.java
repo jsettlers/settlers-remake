f8a544c5a7039e075915389ab4374928e69d18d9
Andreas Eberle <email@andreas-eberle.com>
2017-02-18 22:16:16 +0100

    Fixed bug in PartitionCalculatorAlgorithm that sometimes resulted in invalid partitionings.

--- jsettlers.logic/src/main/java/jsettlers/algorithms/traversing/borders/BorderTraversingAlgorithm.java
+++ jsettlers.logic/src/main/java/jsettlers/algorithms/traversing/borders/BorderTraversingAlgorithm.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016
+ * Copyright (c) 2015 - 2017
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -31,8 +31,7 @@ public final class BorderTraversingAlgorithm {
 	private BorderTraversingAlgorithm() {
 	}
 
-	public static boolean traverseBorder(final IContainingProvider containingProvider, final ShortPoint2D startPos,
-			final IBorderVisitor visitor, boolean visitOutside) {
+	public static boolean traverseBorder(final IContainingProvider containingProvider, final ShortPoint2D startPos, final IBorderVisitor visitor, boolean visitOutside) {
 		return traverseBorder(containingProvider, startPos, visitor, visitOutside, new MutableInt());
 	}
 
