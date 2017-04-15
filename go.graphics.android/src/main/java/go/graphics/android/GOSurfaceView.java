2a98101761cc508ecd3e18347a8b3bb4c72e5aec
Tom Pratt <tom.pratt@live.com>
2017-04-15 17:24:51 +0200

    Added vibrate for selection and tidied up unused c ode

--- go.graphics.android/src/main/java/go/graphics/android/GOSurfaceView.java
+++ go.graphics.android/src/main/java/go/graphics/android/GOSurfaceView.java
@@ -14,17 +14,16 @@
  *******************************************************************************/
 package go.graphics.android;
 
-import go.graphics.GLDrawContext;
-import go.graphics.RedrawListener;
-import go.graphics.UIPoint;
-import go.graphics.area.Area;
-import go.graphics.event.GOEvent;
-import go.graphics.event.GOEventHandlerProvider;
-import go.graphics.event.interpreter.AbstractEventConverter;
+import android.content.Context;
+import android.opengl.GLES10;
+import android.opengl.GLSurfaceView;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 
 import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.Hashtable;
 
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
@@ -32,14 +31,13 @@ import javax.microedition.khronos.egl.EGLContext;
 import javax.microedition.khronos.egl.EGLDisplay;
 import javax.microedition.khronos.opengles.GL10;
 
-import android.content.Context;
-import android.opengl.GLES10;
-import android.opengl.GLSurfaceView;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.InputDevice;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
+import go.graphics.GLDrawContext;
+import go.graphics.RedrawListener;
+import go.graphics.UIPoint;
+import go.graphics.area.Area;
+import go.graphics.event.GOEvent;
+import go.graphics.event.GOEventHandlerProvider;
+import go.graphics.event.interpreter.AbstractEventConverter;
 
 public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 		GOEventHandlerProvider {
@@ -79,7 +77,6 @@ public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 	@Override
 	public boolean onTouchEvent(MotionEvent e) {
 		actionAdapter.onTouchEvent(e);
-
 		return true;
 	}
 
@@ -96,88 +93,46 @@ public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 	}
 
 	private class ActionAdapter extends AbstractEventConverter {
-		private static final double CLICK_MOVE_TRESHOLD = 20;
-		private static final double CLICK_TIME_TRSHOLD = 1;
-		private static final float ZOOMSTART = 2f;
-		private static final double ZOOM_MIN_POINTERDISTANCE = 150;
 
 		protected ActionAdapter(Context context, GOEventHandlerProvider provider) {
 			super(provider);
-//			addReplaceRule(new EventReplacementRule(ReplacableEvent.DRAW,
-//					Replacement.COMMAND_SELECT, CLICK_TIME_TRSHOLD,
-//					CLICK_MOVE_TRESHOLD));
-//			addReplaceRule(new EventReplacementRule(ReplacableEvent.PAN,
-//					Replacement.COMMAND_ACTION, CLICK_TIME_TRSHOLD,
-//					CLICK_MOVE_TRESHOLD));
-
 			gestureDetector = new GestureDetector(context, gestureListener);
 			longPressDetector = new GestureDetector(context, longPressListener);
 			scaleGestureDetector = new ScaleGestureDetector(context, scaleGestureListener);
 
 			gestureDetector.setIsLongpressEnabled(false);
+
+			vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 		}
 
-		private boolean doZoom = false;
-
-		/**
-		 * The points where pointers started. In Android space
-		 */
-		private Hashtable<Integer, UIPoint> panPointerStarts =
-				new Hashtable<Integer, UIPoint>();
-
-		private double endedPansX = 0;
-		private double endedPansY = 0;
+		private final Vibrator vibrator;
 
 		/**
 		 * The pan start center, in GO space
 		 */
 		private UIPoint panStart = new UIPoint(0, 0);
 
-		/**
-		 * Distance the pointers had on zoom start
-		 */
-		private double zoomStartDistance = 0;
-
-		private float lastZoomFactor = 1;
-
 		private final GestureDetector longPressDetector;
 		private final GestureDetector.SimpleOnGestureListener longPressListener = new GestureDetector.SimpleOnGestureListener() {
-
 			@Override
 			public void onLongPress(MotionEvent e) {
-				Log.d("GESTURETEST", "onLongPress SIMPLE");
+				vibrator.vibrate(25);
+
 				endPan(new UIPoint(e.getX() - panStart.getX() ,getHeight() -  e.getY() - panStart.getY()));
 				startDraw(new UIPoint(e.getX(), getHeight() -  e.getY()));
 			}
 		};
 
 		private final GestureDetector gestureDetector;
-		private final GestureDetector.OnGestureListener gestureListener = new GestureDetector.OnGestureListener() {
-			@Override
-			public boolean onDown(MotionEvent e) {
-				Log.d("GESTURETEST", "onDown");
-				return false;
-			}
-
-			@Override
-			public void onShowPress(MotionEvent e) {
-				Log.d("GESTURETEST", "onShowPress");
-
-			}
-
+		private final GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
 			@Override
 			public boolean onSingleTapUp(MotionEvent e) {
-				Log.d("GESTURETEST", "onSingleTapUp");
-
 				fireCommandEvent(new UIPoint(e.getX(), getHeight() -  e.getY()), true);
-
-				return false;
+				return true;
 			}
 
 			@Override
 			public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-				Log.d("GESTURETEST", "onScroll");
-
 				if (drawStarted()) {
 					updateDrawPosition(new UIPoint(e2.getX(), getHeight() -  e2.getY()));
 				} else if (panStarted()) {
@@ -187,50 +142,29 @@ public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 					startPan(new UIPoint(0, 0));
 				}
 
-				return false;
-			}
-
-			@Override
-			public void onLongPress(MotionEvent e) {
-				Log.d("GESTURETEST", "onLongPress");
-
-			}
-
-			@Override
-			public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-				Log.d("GESTURETEST", "onFling");
-				return false;
+				return true;
 			}
 		};
 
-		private UIPoint panPosition = new UIPoint(0,0);
-
 		private final ScaleGestureDetector scaleGestureDetector;
 		private final ScaleGestureDetector.OnScaleGestureListener scaleGestureListener = new ScaleGestureDetector.OnScaleGestureListener() {
-
 			private float startSpan;
 
 			@Override
 			public boolean onScaleBegin(ScaleGestureDetector detector) {
-
 				startSpan = detector.getCurrentSpan();
 				startZoom();
-
-				Log.d("SCALE_GESTURETEST", "onScaleBegin");
 				return true;
 			}
 
 			@Override
 			public boolean onScale(ScaleGestureDetector detector) {
 				updateZoomFactor(detector.getCurrentSpan() / startSpan);
-				Log.d("SCALE_GESTURETEST", "onScale");
 				return true;
 			}
 
 			@Override
 			public void onScaleEnd(ScaleGestureDetector detector) {
-				Log.d("SCALE_GESTURETEST", "onScaleEnd");
-
 				endZoomEvent(detector.getCurrentSpan() / startSpan);
 			}
 		};
@@ -247,12 +181,12 @@ public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 			}
 
 			if (e.getPointerCount() > 1) {
-				if (panStarted()) {
-					endPan(new UIPoint(e.getX() - panStart.getX(), getHeight() - e.getY() - panStart.getY()));
-				}
-
 				if (drawStarted()) {
 					abortDraw();
+				}
+
+				if (panStarted()) {
+					endPan(new UIPoint(e.getX() - panStart.getX(), getHeight() - e.getY() - panStart.getY()));
 				}
 
 				scaleGestureDetector.onTouchEvent(e);
@@ -260,179 +194,11 @@ public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 				gestureDetector.onTouchEvent(e);
 				longPressDetector.onTouchEvent(e);
 			}
-
-		//	temp(e);
-		}
-
-		private void temp(MotionEvent e) {
-
-			boolean isPan =
-					panStarted()
-							|| e.getPointerCount() > 1
-							|| (e.getSource() & InputDevice.SOURCE_CLASS_MASK) == InputDevice.SOURCE_CLASS_TRACKBALL;
-
-			if (isPan) {
-				if (drawStarted()) {
-					abortDraw();
-				}
-				if (!panStarted()) {
-					endedPansX = 0;
-					endedPansY = 0;
-					panStart = getStartedPanAverage();
-					startPan(panStart);
-					zoomStartDistance = getPointerDistance(e);
-					lastZoomFactor = 1;
-				}
-
-				if (e.getAction() == MotionEvent.ACTION_POINTER_DOWN) {
-					Log.d("TEMP", "ACTION_POINTER_DOWN");
-					int index = getPointerIndex(e);
-					Integer id = Integer.valueOf(e.getPointerId(index));
-					UIPoint point = new UIPoint(e.getX(index), e.getY(index));
-					panPointerStarts.put(id, point);
-
-				} else if (e.getAction() == MotionEvent.ACTION_POINTER_UP) {
-					Log.d("TEMP", "ACTION_POINTER_UP");
-					int index = getPointerIndex(e);
-					Integer id = Integer.valueOf(e.getPointerId(index));
-					UIPoint start = panPointerStarts.remove(id);
-					if (start != null) {
-						endedPansX +=
-								(e.getX(index) - start.getX())
-										/ e.getPointerCount();
-						endedPansY -=
-								(e.getY(index) - start.getY())
-										/ e.getPointerCount();
-					}
-
-				} else {
-					float factor = computeZoomFactor(e);
-					if (e.getAction() == MotionEvent.ACTION_MOVE) {
-						UIPoint point = computePanPoint(e);
-						updatePanPosition(point);
-
-						if (e.getPointerCount() > 1
-								&& (factor < 1 / ZOOMSTART || factor > ZOOMSTART)
-								&& getPointerDistance(e) > ZOOM_MIN_POINTERDISTANCE) {
-							doZoom = true;
-							startZoom();
-						}
-
-						if (doZoom) {
-							updateZoomFactor(factor);
-						}
-
-					} else if (e.getAction() == MotionEvent.ACTION_UP) {
-						endPan(computePanPoint(e));
-						if (doZoom) {
-							endZoomEvent(factor);
-						}
-					}
-				}
-			} else {
-				if (e.getAction() == MotionEvent.ACTION_DOWN) {
-					startDraw(convertToLocal(e, 0));
-					panPointerStarts.clear();
-					for (int i = 0; i < e.getPointerCount(); i++) {
-						Integer index = e.getPointerId(i);
-						panPointerStarts.put(index,
-								new UIPoint(e.getX(i), e.getY(i)));
-					}
-				} else if (e.getAction() == MotionEvent.ACTION_MOVE) {
-					updateDrawPosition(convertToLocal(e, 0));
-				} else if (e.getAction() == MotionEvent.ACTION_UP) {
-					endDraw(convertToLocal(e, 0));
-				}
-			}
-		}
-
-		private double getPointerDistance(MotionEvent e) {
-			float centerx = 0;
-			float centery = 0;
-			int pointerCount = e.getPointerCount();
-			for (int i = 0; i < pointerCount; i++) {
-				centerx += e.getX(i);
-				centery += e.getY(i);
-			}
-			centerx /= pointerCount;
-			centery /= pointerCount;
-
-			float dx = 0;
-			float dy = 0;
-			for (int i = 0; i < pointerCount; i++) {
-				dx += Math.abs(centerx - e.getX(i));
-				dy += Math.abs(centery - e.getY(i));
-			}
-			return Math.hypot(dx, dy);
-		}
-
-		private float computeZoomFactor(MotionEvent e) {
-			if (e.getPointerCount() > 1) {
-				double currentDistance = getPointerDistance(e);
-				float zoom = (float) (currentDistance / zoomStartDistance);
-				lastZoomFactor = zoom;
-				return zoom;
-			} else {
-				return lastZoomFactor;
-			}
-		}
-
-		private UIPoint computePanPoint(MotionEvent e) {
-			double dx = endedPansX;
-			double dy = endedPansY;
-			for (int i = 0; i < e.getPointerCount(); i++) {
-				Integer id = Integer.valueOf(e.getPointerId(i));
-				UIPoint p = panPointerStarts.get(id);
-				if (p != null) {
-					dx += e.getX(i) - p.getX();
-					dy -= e.getY(i) - p.getY();
-				}
-			}
-
-			UIPoint point =
-					new UIPoint(panStart.getX() + dx / e.getPointerCount(),
-							panStart.getY() + dy / e.getPointerCount());
-			return point;
-		}
-
-		/**
-		 * Gets the center of the points in the hashtable and adds the endedPans
-		 * 
-		 * @return
-		 */
-		private UIPoint getStartedPanAverage() {
-			double x = 0;
-			double y = 0;
-
-			Collection<UIPoint> values = panPointerStarts.values();
-			for (UIPoint start : values) {
-				x += start.getX();
-				y += start.getY();
-			}
-			return new UIPoint(x / values.size(), getHeight() - y
-					/ values.size());
-		}
-
-		private int getPointerIndex(MotionEvent e) {
-			int mask = MotionEvent.ACTION_POINTER_INDEX_MASK;
-			int shift = MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-			return (e.getActionIndex() & mask) >> shift;
-		}
-
-		private UIPoint convertToLocal(MotionEvent e, int index) {
-			return new UIPoint(e.getX(index), getHeight() - e.getY(index));
-		}
-
-		public void fireKey(String key) {
-			startKeyEvent(key);
-			endKeyEvent(key);
-		}
-
-
-		private UIPoint adjustedCoordinates(MotionEvent e) {
-			return new UIPoint(e.getX(), getHeight() - e.getY());
 		}
 	}
+
+
+
 
 	private class Renderer implements GLSurfaceView.Renderer {
 
@@ -495,10 +261,6 @@ public class GOSurfaceView extends GLSurfaceView implements RedrawListener,
 	@Override
 	public void handleEvent(GOEvent event) {
 		area.handleEvent(event);
-	}
-
-	public void fireKey(String key) {
-		actionAdapter.fireKey(key);
 	}
 
 	public GLDrawContext getDrawContext() {
