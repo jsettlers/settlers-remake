fd60a40a177d2a89ae59f1fe4a05ef05567a37d6
Paul Wedeck <paulwedeck@gmail.com>
2018-07-15 16:18:15 +0200

    EGL Backend Improvements (#738)
    * fix egl fullscreen switching and stop initializing egl in a static block
    * check for right egl version before using its features
    * fix two small and stupid bugs

--- go.graphics.swing/src/main/java/go/graphics/swing/contextcreator/EGLContextCreator.java
+++ go.graphics.swing/src/main/java/go/graphics/swing/contextcreator/EGLContextCreator.java
@@ -17,11 +17,11 @@ import go.graphics.swing.GLContainer;
 public class EGLContextCreator extends JAWTContextCreator {
 
 	private static long egl_config;
-	private static long egl_display;
+	private static long egl_display = 0;
 	private long egl_surface;
 	private static long egl_context;
 
-	private long native_drawable;
+	private long native_drawable = 0;
 
 	public EGLContextCreator(GLContainer container) {
 		super(container);
@@ -46,11 +46,20 @@ public class EGLContextCreator extends JAWTContextCreator {
 		}
 	}
 
-	static {
-		egl_display = EGL10.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
-		EGL10.eglInitialize(egl_display, new int[1], new int[1]);
-		EGL12.eglBindAPI(EGL14.EGL_OPENGL_API);
+	@Override
+	protected void initContext() {}
 
+	private void initStatic() {
+		egl_display = EGL10.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+
+		int[] egl_major = new int[1];
+		int[] egl_minor = new int[1];
+
+		EGL10.eglInitialize(egl_display, egl_major, egl_minor);
+
+		if(egl_major[0] == 1 && egl_minor[0] < 4) throw new Error("EGL version is too low (" +egl_major[0]+"."+egl_minor[0] + ")");
+
+		if(!EGL12.eglBindAPI(EGL14.EGL_OPENGL_API)) throw new Error("could not bind OpenGL");
 
 		int[] attrs = {EGL13.EGL_CONFORMANT, EGL14.EGL_OPENGL_BIT,
 				EGL10.EGL_STENCIL_SIZE, 1,
@@ -65,22 +74,25 @@ public class EGLContextCreator extends JAWTContextCreator {
 		int[] ctx_attrs = new int[] {EGL10.EGL_NONE};
 
 		egl_context = EGL10.eglCreateContext(egl_display, egl_config, 0, ctx_attrs);
-
-	}
-
-	@Override
-	protected void initContext() {
-		egl_surface = EGL10.eglCreateWindowSurface(egl_display, egl_config, native_drawable, (IntBuffer)null);
 	}
 
 	@Override
 	protected void createNewSurfaceInfo() {
+		long new_native_drawable = 0;
+
 		if(Platform.get() == Platform.LINUX) {
 			JAWTX11DrawingSurfaceInfo x11surfaceInfo = JAWTX11DrawingSurfaceInfo.create(surfaceinfo.platformInfo());
-			native_drawable = x11surfaceInfo.drawable();
+			new_native_drawable = x11surfaceInfo.drawable();
 		} else if(Platform.get() == Platform.WINDOWS) {
 			JAWTWin32DrawingSurfaceInfo win32surfaceInfo = JAWTWin32DrawingSurfaceInfo.create(surfaceinfo.platformInfo());
-			native_drawable = win32surfaceInfo.hwnd();
+			new_native_drawable = win32surfaceInfo.hwnd();
+		}
+
+		if(native_drawable != new_native_drawable) {
+			if(egl_display == 0) initStatic();
+
+			if(native_drawable != 0) stop();
+			egl_surface = EGL10.eglCreateWindowSurface(egl_display, egl_config, native_drawable = new_native_drawable, (IntBuffer)null);
 		}
 	}
 }
