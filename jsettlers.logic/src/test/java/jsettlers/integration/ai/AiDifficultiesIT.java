6630fd12d92dcf2732324a5648b7feb3db093167
Andreas Eberle <email@andreas-eberle.com>
2019-07-15 21:04:49 +0200

    Fix tests

--- jsettlers.logic/src/test/java/jsettlers/integration/ai/AiDifficultiesIT.java
+++ jsettlers.logic/src/test/java/jsettlers/integration/ai/AiDifficultiesIT.java
@@ -54,7 +54,7 @@ public class AiDifficultiesIT {
 
 	@Test
 	public void easyShouldConquerVeryEasy() throws MapLoadException {
-		holdBattleBetween(EPlayerType.AI_EASY, EPlayerType.AI_VERY_EASY, 80 * MINUTES);
+		holdBattleBetween(EPlayerType.AI_EASY, EPlayerType.AI_VERY_EASY, 90 * MINUTES);
 	}
 
 	@Test
