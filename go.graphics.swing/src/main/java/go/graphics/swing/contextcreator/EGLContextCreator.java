9e0eeb98ae3fa57da9b73a43c0f2c543d183efcc
Paul Wedeck <paulwedeck@gmail.com>
2019-10-13 12:23:59 +0200

    improve use of EGL_KHR_debug

--- go.graphics.swing/src/main/java/go/graphics/swing/contextcreator/EGLContextCreator.java
+++ go.graphics.swing/src/main/java/go/graphics/swing/contextcreator/EGLContextCreator.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2019
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
 package go.graphics.swing.contextcreator;
 
 import org.lwjgl.BufferUtils;
@@ -84,37 +98,39 @@ public class EGLContextCreator extends JAWTContextCreator {
 					KHRDebug.EGL_DEBUG_MSG_CRITICAL_KHR, critical ? EGL10.EGL_TRUE : EGL10.EGL_FALSE,
 					KHRDebug.EGL_DEBUG_MSG_ERROR_KHR, error_arg ? EGL10.EGL_TRUE : EGL10.EGL_FALSE,
 					KHRDebug.EGL_DEBUG_MSG_WARN_KHR, warning ? EGL10.EGL_TRUE : EGL10.EGL_FALSE,
-					KHRDebug.EGL_DEBUG_MSG_INFO_KHR, info ? EGL10.EGL_TRUE : EGL10.EGL_FALSE
+					KHRDebug.EGL_DEBUG_MSG_INFO_KHR, info ? EGL10.EGL_TRUE : EGL10.EGL_FALSE,
+					EGL10.EGL_NONE
 			);
 
-			PointerBuffer bfr = stack.pointers(MemoryUtil.memAddress(debug));
+			PointerBuffer bfr = stack.pointers(MemoryUtil.memAddress(debug), EGL10.EGL_NONE);
 
-			KHRDebug.eglDebugMessageControlKHR(
-					(error, command, messageType, threadLabel, objectLabel, message) -> {
-						String command_str = EGLDebugMessageKHRCallback.getCommand(command);
-						String message_str = EGLDebugMessageKHRCallback.getMessage(message);
 
-						System.out.println("[EGL] Debug Message");
-						System.out.println("    error: " + error);
-						System.out.println("    command: " + command_str);
-						System.out.println("    messageType: " + messageType);
-						System.out.println("    threadLabel: " + threadLabel);
-						System.out.println("    objectLabel: " + objectLabel);
-						System.out.println("    message: " + message_str);
-					}, bfr);
+			try {
+				KHRDebug.eglDebugMessageControlKHR(
+						(error, command, messageType, threadLabel, objectLabel, message) -> {
+							String command_str = EGLDebugMessageKHRCallback.getCommand(command);
+							String message_str = EGLDebugMessageKHRCallback.getMessage(message);
 
+							System.out.println("[EGL] Debug Message");
+							System.out.println("    error: " + error);
+							System.out.println("    command: " + command_str);
+							System.out.println("    messageType: " + messageType);
+							System.out.println("    threadLabel: " + threadLabel);
+							System.out.println("    objectLabel: " + objectLabel);
+							System.out.println("    message: " + message_str);
+						}, bfr);
+			} catch(Throwable thrown) {}
 		}
 	}
 
-	protected void initStatic() {
+	private void initStatic() {
 		if(egl_display != 0) return;
+
+		if(debug) setEGLDebugFunction(true, true, true, true);
 
 		egl_display = EGL10.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
 		EGL10.eglInitialize(egl_display, new int[] {1}, new int[] {1});
 		EGLCapabilities caps = EGL.createDisplayCapabilities(egl_display);
-
-		if(debug && caps.EGL_KHR_debug) setEGLDebugFunction(true, true, true, true);
-
 		if(!caps.EGL14 || !EGL12.eglBindAPI(EGL14.EGL_OPENGL_API)) error("could not bind OpenGL");
 
 		int[] attrs = {EGL13.EGL_CONFORMANT, EGL14.EGL_OPENGL_BIT,
