769ab4613f0f401edb5ccae6a8e2d47829f53e47
Paul Wedeck <paulwedeck@gmail.com>
2020-04-29 20:14:41 +0200

    use different images on dockyard

--- jsettlers.graphics/src/main/java/jsettlers/graphics/map/controls/original/panel/selection/BuildingSelectionContent.java
+++ jsettlers.graphics/src/main/java/jsettlers/graphics/map/controls/original/panel/selection/BuildingSelectionContent.java
@@ -591,7 +591,7 @@ public class BuildingSelectionContent extends AbstractSelectionContent {
 	}
 
 	private BuildingBackgroundPanel createDockyardBuildingContent(BuildingState state) {
-		DockyardSelectionLayout layout = new DockyardSelectionLayout();
+		DockyardSelectionLayout layout = new DockyardSelectionLayout(null, building.getPlayer().getCivilisation());
 		loadPriorityButton(layout.background, layout.priority, state);
 		layout.nameText.setType(building.getBuildingType(), state.isConstruction());
 
